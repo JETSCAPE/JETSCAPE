a54feca2a82d8f4f1928c2dfc49955293613cda3
Dragos Velicanu <velicanu@mit.edu>
2016-10-18 15:40:32 -0400

    protected fix and add Pythia8 header

--- src/external_generators/interface/BaseHardScattering.h
+++ src/external_generators/interface/BaseHardScattering.h
@@ -12,7 +12,7 @@ public :
   // Pretty sure a reference is fine if we want to add particles to the event
   virtual void generatePartons(HepMC::GenEvent & event);
 
-private:
+protected :
   Parameter pset; // some copy of parameters set
 //probably need to keep track of RNG state TODO
   
