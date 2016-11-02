6065e0ff8da405a663ae6f61bf830fb9687b2088
LongGang Pang <lgpang@qq.com>
2016-11-02 17:46:45 +0100

    add linear interopolation to get fluidcellinfo

--- src/fluid_dynamics.cpp
+++ src/fluid_dynamics.cpp
@@ -3,6 +3,7 @@
 // This is written by Longgang Pang and Chun Shen
 
 #include "./fluid_dynamics.h"
+#include "./linear_interpolation.h"
 
 /** make sure the space time point (tau, x, y, eta) is inside
  * evolution history */
@@ -30,20 +31,44 @@ void EvolutionHistory::check_in_range(real tau, real x, real y, real eta){
 }
 
 /** For one given time step id_tau,
- * get FluidCellInfo at spatial point*/
+ * get FluidCellInfo at spatial point (x, y, eta)*/
 FluidCellInfo EvolutionHistory::get_at_time_step(int id_tau,
                                      real x, real y, real eta) {
     int id_x = get_id_x(x);
     int id_y = get_id_y(y);
     int id_eta = get_id_eta(eta);
+    // cijk for idx=i, idy=j and id_eta=k
+    int c000 = cell_index(id_tau, id_x, id_y, id_eta);
+    int c001 = cell_index(id_tau, id_x, id_y, id_eta+1);
+    int c010 = cell_index(id_tau, id_x, id_y+1, id_eta);
+    int c011 = cell_index(id_tau, id_x, id_y+1, id_eta+1);
+    int c100 = cell_index(id_tau, id_x+1, id_y, id_eta);
+    int c101 = cell_index(id_tau, id_x+1, id_y, id_eta+1);
+    int c110 = cell_index(id_tau, id_x+1, id_y+1, id_eta);
+    int c111 = cell_index(id_tau, id_x+1, id_y+1, id_eta+1);
+    real x0 = x_coord(id_x);
+    real x1 = x_coord(id_x + 1);
+    real y0 = y_coord(id_y);
+    real y1 = y_coord(id_y + 1);
+    real eta0 = eta_coord(id_eta);
+    real eta1 = eta_coord(id_eta + 1);
+
+    return trilinear_int(x0, x1, y0, y1, eta0, eta1,
+            data.at(c000), data.at(c001), data.at(c010), data.at(c011),
+            data.at(c100), data.at(c101), data.at(c110), data.at(c111),
+            x, y, eta);
 }
 
+// do interpolation along time direction; we may also need high order
+// interpolation functions 
 FluidCellInfo EvolutionHistory::get(real tau, real x, real y, real eta){
+    check_in_range(tau, x, y, eta);
     int id_tau = get_id_tau(tau);
-    int id_x = get_id_x(x);
-    int id_y = get_id_y(y);
-    int id_eta = get_id_eta(eta);
-
+    real tau0 = tau_coord(id_tau);
+    real tau1 = tau_coord(id_tau + 1);
+    real bulk0 = get_at_time_step(id_tau, x, y, eta);
+    real bulk1 = get_at_time_step(id_tau+1, x, y, eta);
+    return linear_int(tau0, tau1, bulk0, bulk1, tau);
 }
 
 // if EvolutionHistory is not empty, use JetScape get_hydro_info()
