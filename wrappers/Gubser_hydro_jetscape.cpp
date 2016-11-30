6687870fa355aa491d5457104a2d7bab283ce0e4
Chun Shen <chunshen1987@gmail.com>
2016-11-29 20:39:05 -0500

    add Gubser flow class into JETSCAPE

---
+++ wrappers/Gubser_hydro_jetscape.cpp
@@ -0,0 +1,100 @@
+// Copyright @ Chun Shen
+#include <stdio.h>
+#include <sys/stat.h>
+
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include "../src/fluid_dynamics.h"
+#include "./Gubser_hydro_jetscape.h"
+
+#define hbarc 0.19733
+
+using namespace std;
+
+
+GubserHydro::GubserHydro() {
+    // initialize the parameter reader
+    q = 1.0;
+    e_0 = 1.0;
+
+    hydro_status = NOT_START;
+}
+
+
+GubserHydro::~GubserHydro() {}
+
+
+void GubserHydro::initialize_hydro(Parameter parameter_list) {
+    hydro_status = INITIALIZED;
+}
+
+
+void GubserHydro::evolve_hydro() {
+    hydro_status = FINISHED;
+}
+
+
+double GubserHydro::get_temperature(double e_local) {
+    double N_c = 3.;
+    double N_f = 2.5;
+    double T_local = pow(
+        90.0/M_PI/M_PI*e_local/3./(2.*(N_c*N_c - 1.) + 7./2.*N_c*N_f), 0.25);
+    T_local *= hbarc;
+    return(T_local);
+}
+
+
+void GubserHydro::get_hydro_info(real t, real x, real y, real z,
+                                 FluidCellInfo* fluid_cell_info_ptr) {
+    double t_local = static_cast<double>(t);
+    double x_local = static_cast<double>(x);
+    double y_local = static_cast<double>(y);
+    double z_local = static_cast<double>(z);
+        
+    double tau_local = sqrt(t*t - z*z);
+    double r_local = sqrt(x_local*x_local + y_local*y_local);
+
+    double temp = (1. + 2.*q*q*(tau_local*tau_local + r_local*r_local)
+                   + q*q*q*q*pow(tau_local*tau_local - r_local*r_local, 2));
+
+    double e_local = (
+        (e_0/pow(tau_local, 4./3.))*(pow(2.*q, 8./3.))/(pow(temp, 4./3.)));
+    double T_local = get_temperature(e_local);       // GeV
+    e_local *= hbarc;                                // GeV/fm^3
+    double p_local = e_local/3.;                     // GeV/fm^3
+    double s_local = (e_local + p_local)/T_local;    // 1/fm^3
+
+    double kappa = atanh(
+        (2.*q*q*tau_local*r_local)/(1. + q*q*tau_local*tau_local
+                                    + q*q*r_local*r_local));
+    double ux_local = sinh(kappa)*x_local/(r_local + 1e-15);
+    double uy_local = sinh(kappa)*y_local/(r_local + 1e-15);
+    double gamma = sqrt(1. + ux_local*ux_local + uy_local*uy_local);
+    double vx_local = ux_local/gamma;
+    double vy_local = uy_local/gamma;
+    double vz_local = z/t;
+
+    // assign all the quantites to JETSCAPE output
+    // thermodyanmic quantities
+    fluid_cell_info_ptr->energy_density = e_local;
+    fluid_cell_info_ptr->entropy_density = s_local;
+    fluid_cell_info_ptr->temperature = T_local;
+    fluid_cell_info_ptr->pressure = p_local;
+    // QGP fraction
+    fluid_cell_info_ptr->qgp_fraction = 1.0;
+    // chemical potentials
+    fluid_cell_info_ptr->mu_B = 0.0;
+    fluid_cell_info_ptr->mu_C = 0.0;
+    fluid_cell_info_ptr->mu_S = 0.0;
+    // dynamical quantites
+    fluid_cell_info_ptr->vx = vx_local;
+    fluid_cell_info_ptr->vy = vy_local;
+    fluid_cell_info_ptr->vz = vz_local;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            fluid_cell_info_ptr->pi[i][j] = 0.0;
+        }
+    }
+    fluid_cell_info_ptr->bulk_Pi = 0.0;
+}
---
+++ wrappers/Gubser_hydro_jetscape.h
@@ -0,0 +1,28 @@
+// Copyright @ Chun Shen
+#ifndef TEST_GUBSER_HYDRO_JETSCAPE_H_
+#define TEST_GUBSER_HYDRO_JETSCAPE_H_
+
+#include "../src/fluid_dynamics.h"
+
+class GubserHydro: public FluidDynamics {
+    // this is wrapper class for a simple brick
+    // so that it can be used within the JETSCAPE framework
+ private:
+    double q;
+    double e_0;
+
+ public:
+     GubserHydro();
+     ~GubserHydro();
+
+     void initialize_hydro(Parameter parameter_list);
+
+     void evolve_hydro();
+     double get_temperature(double e_local);
+     void get_hydro_info(real t, real x, real y, real z,
+                         FluidCellInfo* fluid_cell_info_ptr);
+     void get_hypersurface(real T_cut, SurfaceCellInfo* surface_list_ptr) {};
+
+};
+
+#endif  // TEST_GUBSER_HYDRO_JETSCAPE_H_
