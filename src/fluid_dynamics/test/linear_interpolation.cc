8337c1d9608aafae274707a76909ca7b4212b49d
LongGang Pang <lgpang@qq.com>
2017-03-23 17:15:10 -0700

    add unit test for JetClass

--- src/fluid_dynamics/test/linear_interpolation.cc
+++ src/fluid_dynamics/test/linear_interpolation.cc
@@ -4,3 +4,8 @@
 TEST(LinearInterpolationTest, TEST_TRUE){
     EXPECT_EQ(0.5, linear_int(0.0, 1.0, 0.0, 1.0, 0.5));
 }
+
+// test code when the type of y is int
+TEST(LinearInterpolationTest, TEST_INT){
+    EXPECT_EQ(0.5, linear_int(0.0, 1.0, 0, 1, 0.5));
+}
