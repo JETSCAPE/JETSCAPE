e7a8fdee700091d8d0f246c83d8deae9b981e5f9
LongGang Pang <lgpang@qq.com>
2017-05-08 16:14:46 -0700

    using centrality range 30-40% to replace 0-10% to speed up the test_jetscpae_ini

--- src/framework/src/test/JetScapeInitial.cc
+++ src/framework/src/test/JetScapeInitial.cc
@@ -8,16 +8,17 @@
 #include "gtest/gtest.h"
 
 TEST(JetscapeInitialTest, TEST_SAMPLE){
-    double cent_low = 0.0;
-    double cent_high = 10.0;
-    double stored_slow = 112.36181125;
-    double stored_shigh = 205.62898671;
+    double cent_low = 30.0;
+    double cent_high = 40.0;
+    double stored_slow = 33.4898493386; 
+    double stored_shigh = 51.671394987;
     double grid_max = 10.0;
     double grid_step = 0.2;
-    auto ini = JetScapeInitial("auau200", cent_low, cent_high,
+    for (int k = 0; k < 100; k++ ) {
+        auto ini = JetScapeInitial("auau200", cent_low, cent_high,
                                grid_max, grid_step);
-
-    EXPECT_EQ(ini.entropy_density_distribution_.size(), 10000);
-    double mul = ini.info_.total_entropy;
-    ASSERT_TRUE(mul >= stored_slow && mul <= stored_shigh);
+        EXPECT_EQ(ini.entropy_density_distribution_.size(), 10000);
+        double mul = ini.info_.total_entropy;
+        ASSERT_TRUE(mul >= stored_slow && mul <= stored_shigh);
+    }
 }
