b302a2f0114eeb5acbe1e2403e1d0eb4105f7baa
LongGang Pang <lgpang@qq.com>
2017-05-07 23:50:02 -0700

    use entropy range for centrality classes for a given collision system

--- src/framework/src/test/JetScapeInitial.cc
+++ src/framework/src/test/JetScapeInitial.cc
@@ -8,9 +8,16 @@
 #include "gtest/gtest.h"
 
 TEST(JetscapeInitialTest, TEST_SAMPLE){
-    auto ini = JetScapeInitial("auau200", 0, 5, 10, 0.2);
+    double cent_low = 0.0;
+    double cent_high = 10.0;
+    double stored_slow = 112.36181125;
+    double stored_shigh = 205.62898671;
+    double grid_max = 10.0;
+    double grid_step = 0.2;
+    auto ini = JetScapeInitial("auau200", cent_low, cent_high,
+                               grid_max, grid_step);
 
     EXPECT_EQ(ini.entropy_density_distribution_.size(), 10000);
     double mul = ini.info_.total_entropy;
-    ASSERT_TRUE(mul >= 100 && mul <= 150);
+    ASSERT_TRUE(mul >= stored_slow && mul <= stored_shigh);
 }
