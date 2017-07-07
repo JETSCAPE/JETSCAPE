59ae03c936c9c1c723ea492f5eb270459d2700e7
LongGang Pang <lgpang@qq.com>
2017-07-07 15:15:34 -0700

    Create trento initial condition from xml configurations
    1. InitialCondition.* is renamed with TrentoInitial.*
    2. Options are added to jetscape_init.xml for trento
    3. Now trento can use configurations in jetscape_init.xml to create new
    initial conditions
    4. One test case is created in test/TrentoInitialFromXML.cc
    5. ToDo: sigslots to connect entropy_density_distribution_ and
    num_of_binary_collisions_ vectors in TrentoInitial to FluidDynamic
    module and HardProcess module

--- src/framework/src/InitialState.cc
+++ src/framework/src/InitialState.cc
@@ -12,17 +12,9 @@
 namespace Jetscape {
 
 InitialState::InitialState(){
-    collEnergy = 13000;
-    temperature = 175; //MeV? Get the units from HepMC?
-    FourVector v(0,0,0,0);
-    initialVtx.set_location(v);
-}
-
-InitialState::InitialState(double collE, Vertex vtx)
-{
-  collEnergy = collE;
-  initialVtx = vtx;
-
+    //FourVector v(0,0,0,0);
+    //initialVtx.set_location(v);
+    //Init();
 }
 
 InitialState::~InitialState()
@@ -30,9 +22,29 @@ InitialState::~InitialState()
 }
 
 void InitialState::Init(){
-    
-    //Set Pythia (or PGun) collision Energy?
-    
+  JetScapeModuleBase::Init();
+
+  INFO<<"Intialize InitialState ..." << GetId() << "...";
+
+  // JetScapeXML::Instance() returns a singleton if jetscape_init.xml is read in.
+  xml_ = JetScapeXML::Instance()->GetXMLRoot()->FirstChildElement("IS" );  
+
+  if ( !xml_ ) {
+    WARN << " : Not a valid JetScape Initial State XML section in file!";
+    exit(-1);
+  } else {
+    xml_->FirstChildElement("grid_max_x")->QueryDoubleText(&grid_max_x_);
+    xml_->FirstChildElement("grid_max_y")->QueryDoubleText(&grid_max_y_);
+    xml_->FirstChildElement("grid_max_z")->QueryDoubleText(&grid_max_z_);
+    xml_->FirstChildElement("grid_step_x")->QueryDoubleText(&grid_step_x_);
+    xml_->FirstChildElement("grid_step_y")->QueryDoubleText(&grid_step_y_);
+    xml_->FirstChildElement("grid_step_z")->QueryDoubleText(&grid_step_z_);
+    INFO<<"x range for bulk evolution = ["<< -grid_max_x_ <<", "<<grid_max_x_ << "]";
+  }
+
+  JetScapeTask::InitTasks();
+ 
+  //Set Pythia (or PGun) collision Energy?
 }
 
 void InitialState::Exec(){
@@ -44,9 +56,8 @@ void InitialState::Clear(){
 }
 
 void InitialState::Write(weak_ptr<JetScapeWriter> w){
-
     //Write out the original vertex so the writer can keep track of it...
-    w.lock()->Write(make_shared<Vertex>(initialVtx));
+    //w.lock()->Write(make_shared<Vertex>(initialVtx));
 }
 
 } // end namespace Jetscape
--- src/framework/src/test/TrentoInitial.cc
+++ src/framework/src/test/TrentoInitial.cc
@@ -4,7 +4,7 @@
 //                (UC Berkeley and LBNL)
 // -----------------------------------------
 // License and Doxygen-like Documentation to be added ...
-#include "../InitialCondition.h"
+#include "../TrentoInitial.h"
 #include "gtest/gtest.h"
 
 using namespace Jetscape;
@@ -17,10 +17,11 @@ TEST(JetscapeInitialTest, TEST_SAMPLE){
     double grid_max = 10.0;
     double grid_step = 0.2;
     for (int k = 0; k < 100; k++ ) {
-        auto ini = JetScapeInitial("auau200", cent_low, cent_high,
+        auto ini = TrentoInitial("auau200", cent_low, cent_high,
                                grid_max, grid_step);
         EXPECT_EQ(ini.entropy_density_distribution_.size(), 10000);
         double mul = ini.info_.total_entropy;
         ASSERT_TRUE(mul >= stored_slow && mul <= stored_shigh);
     }
 }
+
