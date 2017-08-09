9d3e1935b2a330f5e189b8c15c434d96c208609b
LongGang Pang <lgpang@qq.com>
2017-08-09 10:11:06 -0700

    add the universal random seed to TrentoInitial such that runs with the same random seed produce exactly the same result

--- src/framework/src/TrentoInitial.h
+++ src/framework/src/TrentoInitial.h
@@ -47,14 +47,14 @@ class TrentoInitial : public InitialState {
     // get one random collision in centrality range 0-100%
     void user_defined(std::string projectile, std::string target,
                     double cross_section, double grid_max,
-                    double grid_step);
+                    double grid_step, unsigned random_seed);
 
     // get one random collision in centrality for the given system
     // stored_system = "auau200", "pbpb2760" or "pbpb5020"
     // centrality_range = [centrality_min, centrality_max]
     void pre_defined(std::string stored_system,
                     double centrality_min, double centrality_max,
-                    double grid_max, double grid_step);
+                    double grid_max, double grid_step, unsigned random_seed);
 
     ~TrentoInitial();
 
