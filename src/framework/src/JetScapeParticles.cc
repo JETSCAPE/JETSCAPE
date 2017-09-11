27565d1926b2b4bc2de9350b9008473b61da7a84
Kolja Kauder <kkauder@gmail.com>
2017-09-11 13:42:32 -0400

    avoid unrealistic precision output

--- src/framework/src/JetScapeParticles.cc
+++ src/framework/src/JetScapeParticles.cc
@@ -370,7 +370,7 @@ namespace Jetscape {
     // output<<" x="<<p.x_in().x()<<" y="<<p.x_in().y()<<" z="<<p.x_in().z()<<" t="<<p.x_in().t();
 
     output<<p.plabel()<<" "<<p.pid()<<" "<<p.pstat()<<" ";
-    output<<p.pt()<<" "<<p.rap()<<" "<<p.phi()<<" "<<p.e()<<" ";
+    output<<p.pt()<<" "<< (fabs (p.rap())>1e-15?p.rap():0)<<" "<< p.phi() <<" "<<p.e()<<" ";
     output<<p.x_in().x()<<" "<<p.x_in().y()<<" "<<p.x_in().z()<<" "<<p.x_in().t();//<<endl;
     
     return output;            
