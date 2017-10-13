a9f7015fef6b49cd30cceddd96c7c8ed6b2da10f
Daniel Pablos <peibols@Daniels-MacBook-Air.local>
2017-10-13 17:15:13 -0400

    building strings

--- src/framework/test/PythiaHad.cc
+++ src/framework/test/PythiaHad.cc
@@ -120,6 +120,160 @@ void PythiaHad::DoHadronization(vector<shared_ptr<Parton>>& pIn, vector<shared_p
 {
   INFO<<"Start Hadronizing using Lund string model...";
   event.reset();
+  INFO<<"Number of partons to hadronize: " << pIn.size();
+
+  int col[pIn.size()+1], acol[pIn.size()+1], isdone[pIn.size()+1];
+  memset( col, 0, (pIn.size()+1)*sizeof(int) ), memset( acol, 0, (pIn.size()+1)*sizeof(int) ), memset( isdone, 0, (pIn.size()+1)*sizeof(int) );
+  
+  //Find number of quarks
+  int nquarks=0;
+  int isquark[pIn.size()+1];
+  memset( isquark, 0, (pIn.size()+1)*sizeof(int) );
+  for(unsigned int ipart=0; ipart <  pIn.size(); ++ipart)
+  {
+    if (abs(pIn[ipart]->pid())<=6) {
+      isquark[nquarks]=ipart;
+      nquarks+=1;  
+    }
+  }
+  INFO << "#Quarks = " << nquarks;
+  
+  //Find number of strings
+  int nstrings=int(double(nquarks)/2.+0.6);
+  INFO << "#Strings = " << nstrings;
+
+  //Find ends of strings (order matters in this algo)
+  int istring=0;
+  int one_end[nstrings], two_end[nstrings];
+  for(unsigned int iquark=0; iquark<nquarks; iquark++) {
+    if (isdone[isquark[iquark]]==0) {
+      isdone[isquark[iquark]]=1;
+      one_end[istring]=isquark[iquark];
+      double min_delR=10000.;
+      int partner=-2;
+      for(unsigned int jquark=0; jquark<nquarks; jquark++) {  
+        if (iquark==jquark) continue;
+        int d_jquark=isquark[jquark];
+        if (isdone[d_jquark]==0) {
+          fjcore::PseudoJet pf(pIn[d_jquark]->px(),pIn[d_jquark]->py(),pIn[d_jquark]->pz(),pIn[d_jquark]->e());
+          double delR = pIn[isquark[iquark]]->delta_R(pf);    
+          if (delR<min_delR) min_delR=delR, partner=jquark;
+        }
+      }
+      if (partner!=-2) {
+        isdone[isquark[partner]]=1;
+        two_end[istring]=isquark[partner];
+        istring+=1;
+      }
+      else {
+        FourVector p(0.,0.,1000.,1001.);
+        FourVector x;
+        pIn.push_back(std::make_shared<Parton> (0,1,0,p,x));
+        isquark[nquarks]=pIn.size()-1;
+        nquarks+=1;
+        isdone[pIn.size()-1]=1;
+        two_end[istring]=pIn.size()-1;
+        INFO << " Attached remnant flying down +Pz beam";
+      }
+    }
+  }
+
+  //Assign gluons to a certain string
+  int my_string[pIn.size()];
+  memset( my_string, 0, pIn.size()*sizeof(int) );
+  for(unsigned int ipart=0; ipart <  pIn.size(); ++ipart)
+  {
+    if (pIn[ipart]->pid()==21) {
+      double min_delR=100000.;
+      for (int ns=0; ns<nstrings; ns++)
+      {         
+        int fq=one_end[ns];
+        int sq=two_end[ns];
+        fjcore::PseudoJet pfq(pIn[fq]->px(),pIn[fq]->py(),pIn[fq]->pz(),pIn[fq]->e());
+        double f_delR = pIn[ipart]->delta_R(pfq);
+        fjcore::PseudoJet psq(pIn[sq]->px(),pIn[sq]->py(),pIn[sq]->pz(),pIn[sq]->e());
+        double s_delR = pIn[ipart]->delta_R(psq);
+        double delR=(f_delR+s_delR)/2.;
+        if (delR<min_delR) my_string[ipart]=ns, min_delR=delR;
+      }
+    }
+  }
+
+  //Build up chain using gluons assigned to each string, in a closest pair order
+  int lab_col=102;
+  for (int ns=0; ns<nstrings; ns++)
+  {
+    int tquark=one_end[ns];
+    if (pIn[tquark]->pid()>0) col[tquark]=lab_col;
+    else acol[tquark]=lab_col;
+    lab_col+=1;
+    int link=tquark;
+    int changes=1;
+    do {
+      changes=0;
+      double min_delR=100000.;
+      int next_link=0;
+      for(unsigned int ipart=0; ipart <  pIn.size(); ++ipart)
+      {
+        if (pIn[ipart]->pid()==21 && isdone[ipart]==0 && my_string[ipart]==ns)
+        {
+          changes=1;
+          fjcore::PseudoJet pf(pIn[ipart]->px(),pIn[ipart]->py(),pIn[ipart]->pz(),pIn[ipart]->e());
+          double delR = pIn[link]->delta_R(pf);
+          if (delR<min_delR) min_delR=delR, next_link=ipart;
+        }
+      }
+      if (changes==1)
+      {
+        isdone[next_link]=1;
+        if (col[link]==lab_col-1) col[next_link]=lab_col, acol[next_link]=lab_col-1;
+        else col[next_link]=lab_col-1, acol[next_link]=lab_col;
+        lab_col+=1;
+        cout << " Linked parton= " << next_link << endl;
+        link=next_link;
+      }
+    } while (changes==1);
+    //Attach second end
+    if (col[link]==lab_col-1) col[two_end[ns]]=0, acol[two_end[ns]]=lab_col-1;
+    else col[two_end[ns]]=lab_col-1, acol[two_end[ns]]=0;
+  }
+  //Changing identity of quarks to be consistent with color charge
+  for( int iq=0; iq <  nquarks; ++iq)
+  {
+    if (col[isquark[iq]]!=0) { 
+        if (pIn[isquark[iq]]->pid()<0) pIn[isquark[iq]]->set_id(-pIn[isquark[iq]]->pid());
+      }
+    else {
+      if (pIn[isquark[iq]]->pid()>0) pIn[isquark[iq]]->set_id(-pIn[isquark[iq]]->pid());
+    }
+  }
+
+  /*
+  //Find closest partners:
+  for(unsigned int ipart=0; ipart <  pIn.size(); ++ipart)
+  {
+    //If it is a gluon, need to find two closest partners, if it is a quark only the closest
+    int thisid=0;
+    if (abs(pIn[ipart]->pid())<=6) thisid=1;
+    else if (pIn[ipart]->pid()==21) thisid=2;
+    else continue;
+    double min_delR[2]={100000.};
+    for(unsigned int jpart=0; jpart <  pIn.size(); ++jpart)
+    {
+      if (ipart == jpart) continue;
+      fjcore::PseudoJet pf(pIn[jpart]->px(),pIn[jpart]->py(),pIn[jpart]->pz(),pIn[jpart]->e());  
+      double delR = pIn[ipart]->delta_R(pf);
+      if (delR < min_delR[0]) min_delR[1]=min_delR[0], min_delR[0]=delR, partner[ipart][1]=partner[ipart][0], partner[ipart][0]=jpart;
+      else if (delR < min_delR[1]) min_delR[1]=delR, partner[ipart][1]=jpart;
+      cout << " jpart= " << jpart << endl;
+    }  
+  }
+  */
+
+  for (unsigned int ipart=0; ipart <  pIn.size(); ++ipart)
+  {
+    cout << "Parton #" << ipart << " is a " << pIn[ipart]->pid() << "with energy = " << pIn[ipart]->e() << " with phi= " << pIn[ipart]->phi() << " and has col= " << col[ipart] << " and acol= " << acol[ipart] << endl;
+  }
   for(unsigned int ipart=0; ipart <  pIn.size(); ++ipart)
   {  
     int ide=pIn[ipart]->pid();
@@ -129,15 +283,7 @@ void PythiaHad::DoHadronization(vector<shared_ptr<Parton>>& pIn, vector<shared_p
     double ee=pIn[ipart]->e();
     double mm=pdt.m0(int(ide));
     ee=std::sqrt(px*px+py*py+pz*pz+mm*mm);
-    //Need to provide color singlet object
-    //Dummy setup that provides an antiquark for every quark
-    int col, acol;
-    if (ide>0) col=102, acol=0;
-    else col=0; acol=102;
-    if (ide==22) col=0, acol=0;
-    if (ide==21) ide=22, col=0, acol=0; 
-    event.append(int(ide),23,int(col),int(acol),px,py,pz,ee,mm);
-    event.append(-int(ide),23,int(acol),int(col),-px,-py,-pz,ee,mm); 
+    event.append(int(ide),23,col[ipart],acol[ipart],px,py,pz,ee,mm);
   }
   pythia.next();
   for (unsigned int ipart=0; ipart < event.size(); ++ipart)
