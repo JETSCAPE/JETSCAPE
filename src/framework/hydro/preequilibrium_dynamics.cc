effd6095bca79a7abfe282c617e449bedf91db4e
Derek Everett <derekeverett@gmail.com>
2018-01-19 10:36:30 -0500

    edited files in /framework/hydro for preequilibrium based on fluid_dynamics files

---
+++ src/framework/hydro/preequilibrium_dynamics.cc
@@ -0,0 +1,13 @@
+// Copyright JETSCAPE Collaboration @ 2016
+// This is a general basic class for preequilibrium models
+// This is written by Derek Everett and ...
+
+#include <iostream>
+#include "./preequilibrium_dynamics.h"
+
+using namespace std;
+
+namespace Jetscape {
+
+
+} // end namespace Jetscape
