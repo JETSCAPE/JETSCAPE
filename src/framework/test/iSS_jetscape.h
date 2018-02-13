4f091dacfa9464c13560a9973fbfd905e1aaf6d6
Chun Shen <chunshen1987@gmail.com>
2018-02-12 21:22:12 -0500

    add wrapper for iSS

---
+++ src/framework/test/iSS_jetscape.h
@@ -0,0 +1,26 @@
+// -----------------------------------------
+// This is a wrapper for iSpectraSampler (iSS) with the JETSCAPE framework
+// written by Chun Shen
+// -----------------------------------------
+
+#ifndef TEST_ISS_JETSCAPE_H_
+#define TEST_ISS_JETSCAPE_H_
+
+#include "SoftParticlization.h"
+#include "iSS.h"
+
+using namespace Jetscape;
+
+class iSS_CF: public SoftParticlization {
+ private:
+
+ public:
+    iSS_CF();
+    ~iSS_CF();
+
+    void Init();
+    void Exec();
+    void Clear();
+};
+
+#endif  // TEST_ISS_JETSCAPE_H_
