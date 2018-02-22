a7ece8ba66d4d7e1450e1d941739f15b61762bd3
Kolja Kauder <kkauder@gmail.com>
2018-02-22 15:21:08 -0500

    Upped Verbosity for testing

--- src/framework/src/MUSICTest.cc
+++ src/framework/src/MUSICTest.cc
@@ -63,7 +63,7 @@ int main(int argc, char** argv)
   JetScapeLogger::Instance()->SetRemark(false);
   //SetVerboseLevel (9 a lot of additional debug output ...)
   //If you want to suppress it: use SetVerboseLevel(0) or max  SetVerboseLevel(9) or 10
-  JetScapeLogger::Instance()->SetVerboseLevel(0);
+  JetScapeLogger::Instance()->SetVerboseLevel(5);
    
   Show();
 
