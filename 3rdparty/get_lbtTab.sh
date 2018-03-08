aaad49e3873dac47a1f9740857592af36ec84c95
Kolja Kauder <kkauder@gmail.com>
2018-03-08 11:58:50 -0500

    Allow wget or curl

--- 3rdparty/get_lbtTab.sh
+++ 3rdparty/get_lbtTab.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # download the tables required by LBT code
-wget "https://bitbucket.org/sscao/lbt-tables/downloads/LBT-tables.tar.gz"
+curlcmd=wget
+command -v ${curlcmd} > /dev/null || curlcmd="curl -LO"
+command -v ${curlcmd} > /dev/null || { echo "Please install curl or wget" ; exit 1; }
+$curlcmd "https://bitbucket.org/sscao/lbt-tables/downloads/LBT-tables.tar.gz"
 
 tar xvzf LBT-tables.tar.gz
 
