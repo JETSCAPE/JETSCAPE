34cbe6a6a1ad201b7aeb7d074ad33837bebdbc78
Kolja Kauder <kkauder@gmail.com>
2018-03-15 18:06:06 -0400

    Restored ability to use wget or curl

--- external_packages/get_music.sh
+++ external_packages/get_music.sh
@@ -4,8 +4,13 @@
 #git clone git://git.code.sf.net/p/music-hydro/code music
 
 # solution from JF
-command -v wget > /dev/null || { echo "Please install wget" ; exit 1; }
-wget $(wget -qO- https://sourceforge.net/p/music-hydro/code/ci/public_release_prep/tarball | grep -o "https://sourceforge.net/[a-zA-Z0-9_/.-]*/music-hydro-code-[a-zA-Z0-9]*.zip" | head -1)
+curlcmd1=wget
+curlcmd2="wget -qO-"
+command -v ${curlcmd1} > /dev/null || { curlcmd1="curl -LO"; curlcmd2="curl -s"; }
+command -v ${curlcmd1} > /dev/null || { echo "Please install curl or wget" ; exit 1; }
+
+$curlcmd1 $($curlcmd2 https://sourceforge.net/p/music-hydro/code/ci/public_release_prep/tarball | grep -o "https://sourceforge.net/[a-zA-Z0-9_/.-]*/music-hydro-code-[a-zA-Z0-9]*.zip" | head -1)
+
 unzip music-hydro-code-[a-zA-Z0-9]*.zip > /dev/null
 rm -fr music-hydro-code-[a-zA-Z0-9]*.zip
 ii=`ls | grep "music-hydro-code-[a-zA-Z0-9]*"`
