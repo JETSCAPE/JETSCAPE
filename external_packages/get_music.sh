9f19415b631bb3577b147afdd527f04fb2578928
Chun Shen <chunshen1987@gmail.com>
2018-03-15 16:45:39 -0400

    some small fixes

--- external_packages/get_music.sh
+++ external_packages/get_music.sh
@@ -4,9 +4,7 @@
 #git clone git://git.code.sf.net/p/music-hydro/code music
 
 # solution from JF
-curlcmd=wget
-command -v ${curlcmd} > /dev/null || curlcmd="curl -LO"
-command -v ${curlcmd} > /dev/null || { echo "Please install curl or wget" ; exit 1; }
+command -v wget > /dev/null || { echo "Please install wget" ; exit 1; }
 wget $(wget -qO- https://sourceforge.net/p/music-hydro/code/ci/public_release_prep/tarball | grep -o "https://sourceforge.net/[a-zA-Z0-9_/.-]*/music-hydro-code-[a-zA-Z0-9]*.zip" | head -1)
 unzip music-hydro-code-[a-zA-Z0-9]*.zip > /dev/null
 rm -fr music-hydro-code-[a-zA-Z0-9]*.zip
