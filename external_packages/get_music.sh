a0554c85fa20434f9d97f3d3e2fd4b8677a4ea26
Jean-francois Paquet <jp401@grads-33.internal.phy.duke.edu>
2018-03-19 18:14:30 -0400

    Rewrote get_music.sh a bit to reduce the possibility files would be renamed or removed by mistake

--- external_packages/get_music.sh
+++ external_packages/get_music.sh
@@ -3,17 +3,22 @@
 # download the code package
 #git clone git://git.code.sf.net/p/music-hydro/code music
 
+# Not sure if we want to delete any MUSIC version already there...
 rm -rf music
-rm -rf music-hydro-code-[a-zA-Z0-9]*
 # solution from JF
 curlcmd1=wget
 curlcmd2="wget -qO-"
 command -v ${curlcmd1} > /dev/null || { curlcmd1="curl -LOk"; curlcmd2="curl -sk"; }
 command -v ${curlcmd1} > /dev/null || { echo "Please install curl or wget" ; exit 1; }
 
-$curlcmd1 $($curlcmd2 https://sourceforge.net/p/music-hydro/code/ci/public_release_prep/tarball | grep -o "https://sourceforge.net/[a-zA-Z0-9_/.-]*/music-hydro-code-[a-zA-Z0-9]*.zip" | head -1)
+downloadpath=$($curlcmd2 https://sourceforge.net/p/music-hydro/code/ci/public_release_prep/tarball | grep -o "https://sourceforge.net/[a-zA-Z0-9_/.-]*/music-hydro-code-[a-zA-Z0-9]*.zip" | head -1)
+filename="${downloadpath##*/}"
+filenameWithoutExt="${filename%%.*}"
 
-unzip music-hydro-code-[a-zA-Z0-9]*.zip > /dev/null
-rm -fr music-hydro-code-[a-zA-Z0-9]*.zip
-ii=`ls | grep "music-hydro-code-[a-zA-Z0-9]*"`
-mv $ii music
+# Download file with either curl or wget
+$curlcmd1 ${downloadpath}
+
+# Put file in right location
+unzip ${filename} > /dev/null
+rm -f ${filename}
+mv ${filenameWithoutExt} music
