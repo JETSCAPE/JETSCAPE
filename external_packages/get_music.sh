398fc7d602b94aa1ad5529d89975ccb175be0072
Kolja Kauder <kkauder@gmail.com>
2018-03-19 15:31:45 -0400

    Cleanup for broken attempts. Added k flag to curl to override ssl settings

--- external_packages/get_music.sh
+++ external_packages/get_music.sh
@@ -3,10 +3,12 @@
 # download the code package
 #git clone git://git.code.sf.net/p/music-hydro/code music
 
+rm -rf music
+rm -rf music-hydro-code-[a-zA-Z0-9]*
 # solution from JF
 curlcmd1=wget
 curlcmd2="wget -qO-"
-command -v ${curlcmd1} > /dev/null || { curlcmd1="curl -LO"; curlcmd2="curl -s"; }
+command -v ${curlcmd1} > /dev/null || { curlcmd1="curl -LOk"; curlcmd2="curl -sk"; }
 command -v ${curlcmd1} > /dev/null || { echo "Please install curl or wget" ; exit 1; }
 
 $curlcmd1 $($curlcmd2 https://sourceforge.net/p/music-hydro/code/ci/public_release_prep/tarball | grep -o "https://sourceforge.net/[a-zA-Z0-9_/.-]*/music-hydro-code-[a-zA-Z0-9]*.zip" | head -1)
