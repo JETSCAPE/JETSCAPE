ad3955ac0bb123649f828bab5692a1cf337478c6
Kolja Kauder <kkauder@gmail.com>
2018-03-20 14:32:29 -0400

    Transfer done. Some bugs left.

---
+++ external_packages/get_freestream-milne.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# download the 3+1D OSU freestreaming code
+git clone https://github.com/derekeverett/freestream-milne.git freestream-milne
+#cd freestream-milne
+#git checkout double_prec
