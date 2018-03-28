6d5a75471c0ae436372ddc78eeee4547aab53877
Kolja Kauder <kkauder@gmail.com>
2018-03-27 21:12:43 -0400

    Fixed annoying warnings

--- src/preequilibrium/FreestreamMilneWrapper.h
+++ src/preequilibrium/FreestreamMilneWrapper.h
@@ -9,16 +9,15 @@ using namespace Jetscape;
 
 class FreestreamMilneWrapper: public PreequilibriumDynamics {
  private:
-    int mode; //!< records running mode
-    FREESTREAMMILNE *fsmilne_ptr;
-
+  //int mode; //!< records running mode
+  FREESTREAMMILNE *fsmilne_ptr;
+  
  public:
-    FreestreamMilneWrapper();
-    ~FreestreamMilneWrapper();
-
-    void initialize_preequilibrium(PreEquilibriumParameterFile parameter_list);
-
-    void evolve_preequilibrium();
+  FreestreamMilneWrapper();
+  ~FreestreamMilneWrapper();
+  
+  void initialize_preequilibrium(PreEquilibriumParameterFile parameter_list);  
+  void evolve_preequilibrium();
 };
 
 #endif  // FREESTREAMMILNEWRAPPER_H
