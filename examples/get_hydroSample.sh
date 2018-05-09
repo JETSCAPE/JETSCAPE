1fc2efe07565bfc97d4e6afd8aec52ea3103b6a6
Ebrahim Khalaj <mo.eb.khalaj@gmail.com>
2018-05-09 17:19:14 -0400

    minor fixes

--- examples/get_hydroSample.sh
+++ examples/get_hydroSample.sh
@@ -4,7 +4,9 @@
 # Copyright (c) The JETSCAPE Collaboration, 2018
 #
 # For the list of contributors see AUTHORS.
- *
+#
+# Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+#
 # or via email to bugs.jetscape@gmail.com
 #
 # Distributed under the GNU General Public License 3.0 (GPLv3 or later).
--- src/jet/AdSCFT.cc
+++ src/jet/AdSCFT.cc
@@ -4,7 +4,9 @@
 * Modular, task-based framework for simulating all aspects of heavy-ion collisions
 * 
 * For the list of contributors see AUTHORS.
- *
+*
+* Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+*
 * or via email to bugs.jetscape@gmail.com
 *
 * Distributed under the GNU General Public License 3.0 (GPLv3 or later).
