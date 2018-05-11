318ab410735c5605cd813cd17bad6f2856f9af63
mekhalaj <mo.eb.khalaj@gmail.com>
2018-05-11 13:43:43 -0400

    Add example files

---
+++ examples/LBT_brickTest.cc
@@ -0,0 +1,183 @@
+/*******************************************************************************
+ * Copyright (c) The JETSCAPE Collaboration, 2018
+ *
+ * Modular, task-based framework for simulating all aspects of heavy-ion collisions
+ * 
+ * For the list of contributors see AUTHORS.
+ *
+ * Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+ *
+ * or via email to bugs.jetscape@gmail.com
+ *
+ * Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+ * See COPYING for details.
+ ******************************************************************************/
+// ------------------------------------------------------------
+// JetScape Framework Brick Test Program
+// (use either shared library (need to add paths; see setup.csh)
+// (or create static library and link in)
+// -------------------------------------------------------------
+
+#include <iostream>
+#include <time.h>
+
+// JetScape Framework includes ...
+#include "JetScape.h"
+#include "JetEnergyLoss.h"
+#include "JetEnergyLossManager.h"
+#include "JetScapeWriterStream.h"
+#ifdef USE_HEPMC
+#include "JetScapeWriterHepMC.h"
+#endif
+
+// User modules derived from jetscape framework clasess
+// to be used to run Jetscape ...
+#include "AdSCFT.h"
+#include "Matter.h"
+#include "LBT.h"
+#include "Martini.h"
+#include "Brick.h"
+#include "GubserHydro.h"
+#include "PGun.h"
+#include "PartonPrinter.h"
+#include "HadronizationManager.h"
+#include "Hadronization.h"
+#include "ColoredHadronization.h"
+#include "ColorlessHadronization.h"
+
+// Add initial state module for test
+#include "TrentoInitial.h"
+
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+using namespace Jetscape;
+
+// Forward declaration
+void Show();
+
+// -------------------------------------
+
+int main(int argc, char** argv)
+{
+  clock_t t; t = clock();
+  time_t start, end; time(&start);
+  
+  cout<<endl;
+    
+  // DEBUG=true by default and REMARK=false
+  // can be also set also via XML file (at least partially)
+  JetScapeLogger::Instance()->SetDebug(false);
+  JetScapeLogger::Instance()->SetRemark(false);
+  //SetVerboseLevel (9 a lot of additional debug output ...)
+  //If you want to suppress it: use SetVerboseLevle(0) or max  SetVerboseLevle(9) or 10
+  JetScapeLogger::Instance()->SetVerboseLevel(8);
+   
+  Show();
+
+  auto jetscape = make_shared<JetScape>("./jetscape_init.xml",3);
+  jetscape->SetId("primary");
+  auto jlossmanager = make_shared<JetEnergyLossManager> ();
+  auto jloss = make_shared<JetEnergyLoss> ();
+
+  auto trento = make_shared<TrentoInitial>();
+
+  auto hydro = make_shared<Brick> ();
+  //auto hydro = make_shared<GubserHydro> ();
+  
+  //auto matter = make_shared<Matter> ();
+  auto lbt = make_shared<LBT> ();
+  //auto martini = make_shared<Martini> ();
+  //auto adscft = make_shared<AdSCFT> ();
+  //DBEUG: Remark:
+  //does not matter unfortunately since not called recursively, done by JetEnergyLoss class ...
+  //matter->SetActive(false);
+  //martini->SetActive(false);
+  // This works ... (check with above logic ...)
+  //jloss->SetActive(false);
+
+  auto pGun= make_shared<PGun> ();
+
+  auto printer = make_shared<PartonPrinter> ();
+
+  auto hadroMgr = make_shared<HadronizationManager> ();
+  auto hadro = make_shared<Hadronization> ();
+  auto hadroModule = make_shared<ColoredHadronization> ();
+  auto colorless = make_shared<ColorlessHadronization> ();
+
+  // only pure Ascii writer implemented and working with graph output ...
+  auto writer= make_shared<JetScapeWriterAscii> ("test_out.dat");
+  //autowriter= make_shared<JetScapeWriterAsciiGZ> ("test_out.dat.gz");  
+#ifdef USE_HEPMC
+  //auto writer= make_shared<JetScapeWriterHepMC> ("test_out.hepmc");
+#endif
+  //writer->SetActive(false);
+
+  //Remark: For now modules have to be added
+  //in proper "workflow" order (can be defined via xml and sorted if necessary)
+  
+  jetscape->Add(pGun);
+
+  jetscape->Add(trento);
+
+   //Some modifications will be needed for reusing hydro events, so far
+  //simple test hydros always executed "on the fly" ...
+  jetscape->Add(hydro);
+
+  // Matter with silly "toy shower (no physics)
+  // and Martini dummy ...
+  // Switching Q2 (or whatever variable used
+  // hardcoded at 5 to be changed to xml)
+  //jloss->Add(matter);
+  jloss->Add(lbt);
+  //jloss->Add(martini);
+  //jloss->Add(adscft);  
+
+  jlossmanager->Add(jloss);
+  
+  jetscape->Add(jlossmanager);
+
+  jetscape->Add(printer);
+
+  //hadro->Add(hadroModule);
+  hadro->Add(colorless);
+  hadroMgr->Add(hadro);
+  jetscape->Add(hadroMgr);
+
+  jetscape->Add(writer);
+
+  // Intialize all modules tasks
+  jetscape->Init();
+
+  // Run JetScape with all task/modules as specified ...
+  jetscape->Exec();
+
+  // "dummy" so far ...
+  // Most thinkgs done in write and clear ...
+  jetscape->Finish();
+  
+  INFO_NICE<<"Finished!";
+  cout<<endl;
+
+  // wait for 5s
+  //std::this_thread::sleep_for(std::chrono::milliseconds(500000));
+
+  t = clock() - t;
+  time(&end);
+  printf ("CPU time: %f seconds.\n",((float)t)/CLOCKS_PER_SEC);
+  printf ("Real time: %f seconds.\n",difftime(end,start));
+  //printf ("Real time: %f seconds.\n",(start-end));
+  return 0;
+}
+
+// -------------------------------------
+
+void Show()
+{
+  INFO_NICE<<"------------------------------------";
+  INFO_NICE<<"| Brick Test JetScape Framework ... |";
+  INFO_NICE<<"------------------------------------";
+  INFO_NICE;
+}
---
+++ examples/PythiaBrickTest.cc
@@ -0,0 +1,206 @@
+/*******************************************************************************
+ * Copyright (c) The JETSCAPE Collaboration, 2018
+ *
+ * Modular, task-based framework for simulating all aspects of heavy-ion collisions
+ * 
+ * For the list of contributors see AUTHORS.
+ *
+ * Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+ *
+ * or via email to bugs.jetscape@gmail.com
+ *
+ * Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+ * See COPYING for details.
+ ******************************************************************************/
+// ------------------------------------------------------------
+// JetScape Framework Brick Test Program with Pythia IS
+// (use either shared library (need to add paths; see setup.csh)
+// (or create static library and link in)
+// -------------------------------------------------------------
+
+#include <iostream>
+#include <time.h>
+
+// JetScape Framework includes ...
+#include "JetScape.h"
+#include "JetEnergyLoss.h"
+#include "JetEnergyLossManager.h"
+#include "JetScapeWriterStream.h"
+#ifdef USE_HEPMC
+#include "JetScapeWriterHepMC.h"
+#endif
+
+// User modules derived from jetscape framework clasess
+// to be used to run Jetscape ...
+#include "AdSCFT.h"
+#include "Matter.h"
+#include "Martini.h"
+#include "Brick.h"
+#include "GubserHydro.h"
+#include "PythiaGun.h"
+#include "PartonPrinter.h"
+#include "HadronizationManager.h"
+#include "Hadronization.h"
+#include "ColoredHadronization.h"
+#include "ColorlessHadronization.h"
+
+// Add initial state module for test
+#include "TrentoInitial.h"
+
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+using namespace Jetscape;
+
+// Forward declaration
+void Show();
+
+// -------------------------------------
+
+int main(int argc, char** argv)
+{
+  clock_t t; t = clock();
+  time_t start, end; time(&start);
+
+  cout<<endl;
+    
+  // DEBUG=true by default and REMARK=false
+  // can be also set also via XML file (at least partially)
+  JetScapeLogger::Instance()->SetInfo(true);
+  JetScapeLogger::Instance()->SetDebug(false);
+  JetScapeLogger::Instance()->SetRemark(false);
+  //SetVerboseLevel (9 a lot of additional debug output ...)
+  //If you want to suppress it: use SetVerboseLevle(0) or max  SetVerboseLevle(9) or 10
+  JetScapeLogger::Instance()->SetVerboseLevel(0);
+
+  
+  Show();
+
+  auto jetscape = make_shared<JetScape>("./jetscape_init.xml",20);
+  jetscape->SetId("primary");
+  // jetscape->set_reuse_hydro (true);
+  // jetscape->set_n_reuse_hydro (10);
+
+  auto jlossmanager = make_shared<JetEnergyLossManager> ();
+  auto jloss = make_shared<JetEnergyLoss> ();
+  auto trento = make_shared<TrentoInitial>();
+  auto hydro = make_shared<Brick> ();
+  //auto hydro = make_shared<GubserHydro> ();
+  
+  auto matter = make_shared<Matter> ();
+  auto martini = make_shared<Martini> ();
+  auto adscft = make_shared<AdSCFT> ();
+
+  auto pythiaGun= make_shared<PythiaGun> ();
+
+  auto printer = make_shared<PartonPrinter>();
+  auto hadroMgr = make_shared<HadronizationManager> ();
+  auto hadro = make_shared<Hadronization> ();
+  auto hadroModule = make_shared<ColoredHadronization> ();
+  auto colorless = make_shared<ColorlessHadronization> ();
+  
+  // only pure Ascii writer implemented and working with graph output ...
+  auto writer= make_shared<JetScapeWriterAscii> ("test_out.dat");
+  auto writergz= make_shared<JetScapeWriterAsciiGZ> ("test_out.dat.gz");
+  //writer->SetActive(false);
+
+  //Remark: For now modules have to be added
+  //in proper "workflow" order (can be defined via xml and sorted if necessary)  
+  jetscape->Add(trento);
+  jetscape->Add(pythiaGun);
+
+  //Some modifications will be needed for reusing hydro events, so far
+  //simple test hydros always executed "on the fly" ...
+  jetscape->Add(hydro);
+
+  jloss->Add(matter);
+  // jloss->Add(martini);
+  // jloss->Add(adscft);
+
+  jlossmanager->Add(jloss);
+  
+  jetscape->Add(jlossmanager);
+
+  jetscape->Add(printer);
+  hadro->Add(hadroModule);
+  // hadro->Add(colorless);
+  hadroMgr->Add(hadro);
+  jetscape->Add(hadroMgr);
+
+  jetscape->Add(writer);
+  jetscape->Add(writergz);
+#ifdef USE_HEPMC
+  auto hepmcwriter= make_shared<JetScapeWriterHepMC> ("test_out.hepmc");
+  jetscape->Add(hepmcwriter);
+#endif
+
+  // Intialize all modules tasks
+  jetscape->Init();
+  
+  // Run JetScape with all task/modules as specified ...
+  jetscape->Exec();
+
+  // "dummy" so far ...
+  // Most thinkgs done in write and clear ...
+  jetscape->Finish();
+  
+  INFO_NICE<<"Finished!";
+  cout<<endl;
+
+  // Some information is only known after the full run,
+  // Therefore store information at the end of the file, in a footer
+  writer->WriteComment ( "EVENT GENERATION INFORMATION" );
+  Pythia8::Info& info = pythiaGun->info;
+  std::ostringstream oss;
+  oss.str(""); oss << "Total nTried    = " << info.nTried();
+  writer->WriteComment ( oss.str() );
+  oss.str(""); oss << "Total nSelected = " << info.nSelected();
+  writer->WriteComment ( oss.str() );
+  oss.str(""); oss << "Total nAccepted = " << info.nAccepted();
+  writer->WriteComment ( oss.str() );
+  oss.str(""); oss << "Total sigmaGen  = " << info.sigmaGen();  
+  writer->WriteComment ( oss.str() );
+  oss.str(""); oss << "Total sigmaErr  = " << info.sigmaErr();
+  writer->WriteComment ( oss.str() );
+
+  oss.str(""); oss << "eCM  = " << info.eCM();
+  writer->WriteComment ( oss.str() );
+  oss.str(""); oss << "pTHatMin  = " << pythiaGun->GetpTHatMin();
+  writer->WriteComment ( oss.str() );
+  oss.str(""); oss << "pTHatMax  = " << pythiaGun->GetpTHatMax();
+  //writer->WriteComment ( oss.str() );
+  //oss.str(""); oss << "JETSCAPE Random Seed  = " << JetScapeTaskSupport::Instance()->GetRandomSeed();
+  writer->WriteComment ( oss.str() );
+  writer->WriteComment ( "/EVENT GENERATION INFORMATION" );
+
+  t = clock() - t;
+  time(&end);
+  printf ("CPU time: %f seconds.\n",((float)t)/CLOCKS_PER_SEC);
+  printf ("Real time: %f seconds.\n",difftime(end,start));
+
+  // Print pythia statistics
+  // pythiaGun->stat();
+
+  // Demonstrate how to work with pythia statistics
+  //Pythia8::Info& info = pythiaGun->info;
+  cout << " nTried    = " << info.nTried() << endl;
+  cout << " nSelected = " << info.nSelected()  << endl;
+  cout << " nAccepted = " << info.nAccepted()  << endl;
+  cout << " sigmaGen  = " <<   info.sigmaGen()  << endl;  
+  cout << " sigmaErr  = " <<   info.sigmaErr()  << endl;
+ 
+  
+  return 0;
+}
+
+// -------------------------------------
+
+void Show()
+{
+  INFO_NICE<<"------------------------------------";
+  INFO_NICE<<"| Brick Test JetScape Framework ... |";
+  INFO_NICE<<"------------------------------------";
+  INFO_NICE;
+}
