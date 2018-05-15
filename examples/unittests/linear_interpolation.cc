32e92f49c21ce0951c83a96414e67dad8b858255
mekhalaj <mo.eb.khalaj@gmail.com>
2018-05-15 19:06:20 -0400

    Add files via upload

---
+++ examples/unittests/linear_interpolation.cc
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) The JETSCAPE Collaboration, 2018
+ *
+ * Modular, task-based framework for simulating all aspects of heavy-ion collisions
+ * 
+ * For the list of contributors see AUTHORS.
+ *
+ * Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+ *
+ * or via email to bugs.jetscape@gmail.com
+ *
+ * Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+ * See COPYING for details.
+ ******************************************************************************/
+
+#include "../linear_interpolation.h"
+#include "gtest/gtest.h"
+
+using namespace Jetscape;
+
+TEST(LinearInterpolationTest, TEST_TRUE){
+    EXPECT_EQ(0.5, linear_int(0.0, 1.0, 0.0, 1.0, 0.5));
+}
+
+// test code when the type of y is int
+TEST(LinearInterpolationTest, TEST_INT){
+    EXPECT_EQ(0, linear_int(0.0, 1.0, 0, 1, 0.5));
+}
