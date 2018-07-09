b18b5b2895f339d8f7317d2363d6fdfed2bc70ab
LongGang Pang <lgpang@qq.com>
2018-07-09 16:44:36 -0700

    add script to download clvisc from cloud

---
+++ external_packages/get_clvisc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Copyright (c) The JETSCAPE Collaboration, 2018
+#
+# For the list of contributors see AUTHORS.
+#
+# Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+#
+# or via email to bugs.jetscape@gmail.com
+#
+# Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+# See COPYING for details.
+##############################################################################
+
+# download the code package
+git clone git@gitlab.com:snowhitiger/PyVisc.git
