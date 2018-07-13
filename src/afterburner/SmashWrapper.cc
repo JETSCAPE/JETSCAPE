cdadc81603ab886b935047bc5ee0b28e79c26d1c
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2018-07-12 17:16:09 -0700

    Pass hadrons to SMASH and back (not compiling yet)
    This refs #96
    
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- src/afterburner/SmashWrapper.cc
+++ src/afterburner/SmashWrapper.cc
@@ -18,9 +18,7 @@
 
 #include "SmashWrapper.h"
 
-#include "smash/configuration.h"
 #include "smash/decaymodes.h"
-#include "smash/experiment.h"
 #include "smash/inputfunctions.h"
 #include "smash/particles.h"
 
@@ -30,6 +28,12 @@
 #include <boost/filesystem/fstream.hpp>
 
 using namespace Jetscape;
+
+double AfterburnerModus::initial_conditions(smash::Particles *particles,
+                                      const smash::ExperimentParameters &) {
+  backpropagate_to_same_time(*particles);
+  return start_time_;
+}
 
 SmashWrapper::SmashWrapper() {
   SetId("SMASH");
@@ -86,18 +90,95 @@ void SmashWrapper::InitTask() {
   smash::set_default_loglevel(
       config.take({"Logging", "default"}, einhard::INFO));
   smash::create_all_loggers(config["Logging"]);
+  // Read in the rest of configuration
+  float end_time;
+  smash_xml_->FirstChildElement("end_time")->QueryFloatText(&end_time);
+  config["General"]["End_Time"] = end_time;
+  smash_xml_->FirstChildElement("only_decays")->
+      QueryBoolText(&only_final_decays_);
+  JSINFO << "End time for SMASH is set to " << end_time << " fm/c";
+  if (only_final_decays_) {
+    JSINFO << "SMASH will only perform resonance decays, no propagation";
+  }
+  // output path is just dummy here, because no output from SMASH is foreseen
+  JSINFO << "Seting up SMASH Experiment object";
+  boost::filesystem::path output_path("./smash_output");
+  smash_experiment_ = make_shared<smash::Experiment<AfterburnerModus>>(
+      config, output_path);
 }
 
 void SmashWrapper::ExecuteTask() {
-  // todo(oliiny): Is it really avoiding copy?
-  const auto &initial_hadrons = soft_particlization_sampler_->Hadron_list_;
-  const int n_events = initial_hadrons.size();
+  // todo(oliiny): does it help to avoid copying?
+  const auto& JS_initial_hadrons = soft_particlization_sampler_->Hadron_list_;
+  const int n_events = JS_initial_hadrons.size();
   JSINFO << "SMASH: obtained " << n_events << " events from particlization";
+  smash::Particles* smash_particles = smash_experiment_->particles();
   for (unsigned int i = 0; i < n_events; i++) {
-    JSINFO << initial_hadrons[i].size() << " particles in event " << i;
+    JSINFO << "Event " << i << " SMASH starts with " <<
+              JS_initial_hadrons[i].size() << " particles.";
+    JS_hadrons_to_smash_particles(JS_initial_hadrons, *smash_particles);
+    if (!only_final_decays_) {
+      smash_experiment_->initialize_new_event();
+      smash_experiment_->run_time_evolution();
+    }
+    smash_experiment_->do_final_decays();
+    std::vector<shared_ptr<Hadron>> final_hadrons_this_event;
+    smash_particles_to_JS_hadrons(*smash_particles, final_hadrons_this_event);
+    final_hadrons_.push_back(final_hadrons_this_event);
+    JSINFO << final_hadrons_this_event.size() << " hadrons from SMASH.";
   }
 }
 
 void SmashWrapper::WriteTask(weak_ptr<JetScapeWriter> w) {
   JSINFO << "SMASH hadronic afterburner printout";
+  auto f = w.lock();
+  if ( !f ) return;
+
+  f->WriteComment("JetScape module: " + GetId());
+  for (const auto &event : final_hadrons_) {
+    int i = -1;
+    for (const auto hadron : event) {
+      f->WriteWhiteSpace("[" + to_string(++i) + "] H");
+      f->Write(hadron);
+    }
+  }
+}
+
+void SmashWrapper::JS_hadrons_to_smash_particles(
+    std::vector<shared_ptr<Hadron>>& JS_hadrons,
+    smash::Particles& smash_particles) {
+  AfterburnerModus* modus = smash_experiment_->modus();
+  smash_particles.reset();
+  for (const auto JS_hadron : JS_hadrons) {
+    const FourVector p = JS_hadron->p_in();
+    const FourVector r = JS_hadron->x_in();
+    const double mass = JS_hadron->restmass();
+    smash::PdgCode pdgcode = smash::PdgCode::from_decimal(JS_hadron->pid());
+    modus->try_create_particle(smash_particles,
+      pdgcode, r.t(), r.x(), r.y(), r.z(), mass,
+      JS_hadron->e(), JS_hadron->px(), JS_hadron->py(), JS_hadron->pz());
+  }
+}
+
+void SmashWrapper::smash_particles_to_JS_hadrons(
+    const smash::Particles& smash_particles,
+    std::vector<shared_ptr<Hadron>>& JS_hadrons) {
+  JS_hadrons.clear();
+  for (const auto &particle : smash_particles) {
+    const int hadron_label = 0;
+    const int hadron_status = -1;
+    const int hadron_id = particle.pdgcode().get_decimal();
+    smash::FourVector p = particle.momentum(),
+                      r = particle.position();
+    const FourVector hadron_p(p.x1(), p.x2(), p.x3(), p.x0()),
+                     hadron_r(r.x1(), r.x2(), r.x3(), r.x0());
+    const double hadron_mass = p.abs();
+    JS_hadrons.push_back(make_shared<Hadron>(
+         hadron_label,
+         hadron_id,
+         hadron_status,
+         hadron_p,
+         hadron_r,
+         hadron_mass));
+  }
 }
--- src/afterburner/SmashWrapper.h
+++ src/afterburner/SmashWrapper.h
@@ -22,12 +22,41 @@
 #include "Afterburner.h"
 #include "JetScapeWriter.h"
 
+#include "smash/configuration.h"
+#include "smash/experiment.h"
+#include "smash/listmodus.h"
+
 using namespace Jetscape;
+
+/**
+ * An Afterburner modus, which acts similarly to SMASH ListModus, but differs
+ * from it in the initialization.
+ */
+class AfterburnerModus : public smash::ListModus {
+ public:
+  // Unlike for ListModus there is no need to get any data from the config
+  // That is why this constructor is empty
+  AfterburnerModus(smash::Configuration modus_config,
+                   const smash::ExperimentParameters &par) {}
+  // This function overrides the function from ListModus.
+  // It allows to avoid reading any files like ListModus.
+  double initial_conditions(smash::Particles *particles,
+                            const smash::ExperimentParameters &par);
+};
 
 class SmashWrapper: public Afterburner {
  private:
+    std::vector<std::vector<shared_ptr<Hadron>>> final_hadrons_;
     tinyxml2::XMLElement *smash_xml_;
+    bool only_final_decays_ = false;
+    shared_ptr<smash::Experiment<AfterburnerModus>> smash_experiment_;
 
+    void smash_particles_to_JS_hadrons(const smash::Particles& smash_particles,
+                                  std::vector<shared_ptr<Hadron>>& JS_hadrons);
+
+    void JS_hadrons_to_smash_particles(
+        const std::vector<shared_ptr<Hadron>>& JS_hadrons,
+        smash::Particles& smash_particles);
  public:
     SmashWrapper();
     void InitTask();
