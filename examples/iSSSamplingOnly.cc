147f6947480580b113a2e34c063e939d53413e76
Derek Everett <everett@login1.stampede2.tacc.utexas.edu>
2018-12-07 10:36:17 -0600

    checking if up to date

---
+++ examples/iSSSamplingOnly.cc
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (c) The JETSCAPE Collaboration, 2018
+ *
+ * Modular, task-based framework for simulating all aspects of heavy-ion collisions
+ * 
+ * For the list of contributors see AUTHORS.
+ *
+ * Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+ *
+ * or via email to bugs.jetscape@gmail.com
+ *
+ * Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+ * See COPYING for details.
+ ******************************************************************************/
+// ------------------------------------------------------------
+// JetScape Framework hydro from file Test Program
+// (use either shared library (need to add paths; see setup.csh)
+// (or create static library and link in)
+// -------------------------------------------------------------
+
+#include <iostream>
+#include <time.h>
+
+// JetScape Framework includes ...
+#include "JetScape.h"
+#include "JetEnergyLoss.h"
+#include "JetEnergyLossManager.h"
+#include "JetScapeWriterStream.h"
+#ifdef USE_HEPMC
+#include "JetScapeWriterHepMC.h"
+#endif
+
+// User modules derived from jetscape framework clasess
+#include "AdSCFT.h"
+#include "Matter.h"
+#include "Martini.h"
+#include "MusicWrapper.h"
+#include "iSpectraSamplerWrapper.h"
+#include "TrentoInitial.h"
+#include "PGun.h"
+#include "PartonPrinter.h"
+#include "HadronizationManager.h"
+#include "Hadronization.h"
+#include "ColoredHadronization.h"
+
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+using namespace Jetscape;
+
+// Forward declaration
+void Show();
+
+// -------------------------------------
+
+int main(int argc, char** argv)
+{
+  clock_t t; t = clock();
+  time_t start, end; time(&start);
+  
+  cout<<endl;
+    
+  // DEBUG=true by default and REMARK=false
+  // can be also set also via XML file (at least partially)
+  JetScapeLogger::Instance()->SetInfo(true);
+  JetScapeLogger::Instance()->SetDebug(true);
+  JetScapeLogger::Instance()->SetRemark(false);
+  //SetVerboseLevel (9 a lot of additional debug output ...)
+  //If you want to suppress it: use SetVerboseLevel(0) or max  SetVerboseLevel(9) or 10
+  JetScapeLogger::Instance()->SetVerboseLevel(8);
+   
+  Show();
+
+  auto jetscape = make_shared<JetScape>("./jetscape_init.xml",1);
+  jetscape->SetReuseHydro (false);
+  jetscape->SetNReuseHydro (0);
+
+  // surface sampler
+  auto iSS = make_shared<iSpectraSamplerWrapper> ();
+  jetscape->Add(iSS);
+
+  // Output
+  auto writer= make_shared<JetScapeWriterAscii> ("test_out.dat");
+  // same as JetScapeWriterAscii but gzipped
+  // auto writer= make_shared<JetScapeWriterAsciiGZ> ("test_out.dat.gz");
+  // HEPMC3
+#ifdef USE_HEPMC
+  // auto writer= make_shared<JetScapeWriterHepMC> ("test_out.hepmc");
+#endif
+  jetscape->Add(writer);
+
+  // Intialize all modules tasks
+  jetscape->Init();
+
+  // Run JetScape with all task/modules as specified ...
+  jetscape->Exec();
+
+  // "dummy" so far ...
+  // Most thinkgs done in write and clear ...
+  jetscape->Finish();
+  
+  INFO_NICE<<"Finished!";
+  cout<<endl;
+
+  t = clock() - t;
+  time(&end);
+  printf ("CPU time: %f seconds.\n",((float)t)/CLOCKS_PER_SEC);
+  printf ("Real time: %f seconds.\n",difftime(end,start));
+  return 0;
+}
+
+// -------------------------------------
+
+void Show()
+{
+  INFO_NICE<<"-----------------------------------------------";
+  INFO_NICE<<"| iSSSamplingOnly JetScape Framework ... |";
+  INFO_NICE<<"-----------------------------------------------";
+  INFO_NICE;
+}
