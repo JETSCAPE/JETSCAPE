538a52f6c89c8b52c6bde23516ab155f233817b5
Chun Shen <chunshen1987@gmail.com>
2019-04-24 17:58:37 -0400

    do not run surfaceFinder in the twostage hydro

--- src/hydro/MusicWrapper.cc
+++ src/hydro/MusicWrapper.cc
@@ -118,7 +118,7 @@ void MpiMusic::EvolveHydro() {
                << bulk_info.data.size();
         music_hydro_ptr->clear_hydro_info_from_memory();
 
-        FindAConstantTemperatureSurface(0.16);
+        //FindAConstantTemperatureSurface(0.16);
     }
     
     collect_freeze_out_surface();
