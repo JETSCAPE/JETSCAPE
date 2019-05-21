75ec312245a4662988b025641789f8fdab8f09e8
Abhijit Majumder <abhijit.majumder@gmail.com>
2019-05-21 17:04:31 -0400

    Energy non-conservation issues fixed.
    Defined t = E^2 - px^2 -py^2 -pz^2 - m^2 in JetScapeParticles.cc
    Typos in MATTER.cc formulas fixed.
    Removed 2 attachments of TRENTO in bricktest.cc, was leading to memory explosion.
    Added a pythia helper in PGun to get the mass of the particle and be consistent with JetScapeParticleBase
    Initialized tranport coefficients in MATTER

--- src/framework/JetScapeParticles.cc
+++ src/framework/JetScapeParticles.cc
@@ -405,7 +405,11 @@ namespace Jetscape {
     /// \Todo: Fix
     //  double t_parton = PseudoJet::m2()  - restmass()*restmass() ;
       double t_parton = e()*e() - px()*px() - py()*py() - pz()*pz() - restmass()*restmass();
-      if (t_parton< 0.0) JSWARN << " Virtuality is negative, MATTER cannot handle these particles " << " t = " << t_parton;
+      if (t_parton< 0.0)
+      {
+          JSWARN << " Virtuality is negative, MATTER cannot handle these particles " << " t = " << t_parton;
+          JSWARN << " pid = "<< pid() << " E = " << e() << " px = " << px() << " py = " << py() << " pz = " << pz() ;
+      }
     return ( t_parton ) ;
     // return (t_) ;
   }        
