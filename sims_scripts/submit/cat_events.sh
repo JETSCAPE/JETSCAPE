81dc67197bd59efe3eb73e1ffdb67bbfd030de3a
Derek Everett <everett@login4.stampede2.tacc.utexas.edu>
2019-08-28 16:15:37 -0500

    making sure scripts to run events are updated

--- sims_scripts/submit/cat_events.sh
+++ sims_scripts/submit/cat_events.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
-for i in {0..99}; do
-    echo "catting $i" 
-    cat $SCRATCH/results/$1/$i/[0-9]*.dat >> CAT-RESULTS/$i.dat
+job_id = $1
+
+for design_pt in {0..99}; do
+    echo "catting $design_pt"
+    cd CAT-RESULTS
+    mkdir $job_id
+    cat $SCRATCH/results/$job_id/$design_pt/[0-9]*.dat >> CAT-RESULTS/$job_id/$design_pt.dat
 done
