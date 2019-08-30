4ae164cf181aee5799cbc86543931b7eb7cef999
Weiyao Ke <wk42@login3.stampede2.tacc.utexas.edu>
2019-08-30 11:26:50 -0500

    add a submit validation script

--- sims_scripts/submit/cat_events.sh
+++ sims_scripts/submit/cat_events.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-for i in {0..99}; do
+#for i in {0..200}; do
+#    echo "catting $i" 
+#    cat $SCRATCH/results/*/$i/[0-9]* > CAT-RESULTS/$i.dat
+#done
+
+for i in {0..49}; do
     echo "catting $i" 
-    cat $SCRATCH/results/*/$i/[0-9]* > CAT-RESULTS/$i.dat
+    cat $SCRATCH/results-validation/4114650/$i/[0-9]* > CAT-RESULTS-VALID/$i.dat
 done
