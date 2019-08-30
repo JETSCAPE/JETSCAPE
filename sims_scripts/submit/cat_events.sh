d3ca1f0d8805316fa41328504279995ead44bb51
Weiyao Ke <wk42@login3.stampede2.tacc.utexas.edu>
2019-08-30 12:44:50 -0500

    revert cat_event.sh and submit script

--- sims_scripts/submit/cat_events.sh
+++ sims_scripts/submit/cat_events.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
 
-#for i in {0..200}; do
-#    echo "catting $i" 
-#    cat $SCRATCH/results/*/$i/[0-9]* > CAT-RESULTS/$i.dat
-#done
+job_id = $1
 
-for i in {0..49}; do
-    echo "catting $i" 
-    cat $SCRATCH/results-validation/4114650/$i/[0-9]* > CAT-RESULTS-VALID/$i.dat
+for design_pt in {0..99}; do
+    echo "catting $design_pt"
+    cd CAT-RESULTS
+    mkdir $job_id
+    cat $SCRATCH/results/$job_id/$design_pt/[0-9]*.dat >> CAT-RESULTS/$job_id/$design_pt.dat
 done
