a20f71f62366c6a9c7a96de172198372f44ba6ff
Derek Everett <everett@login3.stampede2.tacc.utexas.edu>
2019-08-30 16:09:14 -0500

    some scripts to run Au Au events

--- sims_scripts/submit/cat_events.sh
+++ sims_scripts/submit/cat_events.sh
@@ -4,7 +4,7 @@ job_id = $1
 
 for design_pt in {0..99}; do
     echo "catting $design_pt"
-    cd CAT-RESULTS
+    cd catted_results
     mkdir $job_id
-    cat $SCRATCH/results/$job_id/$design_pt/[0-9]*.dat >> CAT-RESULTS/$job_id/$design_pt.dat
+    cat $SCRATCH/results/$job_id/$design_pt/[0-9]*.dat >> catted_results/$job_id/$design_pt.dat
 done
