ee83911a6053427ab1fca33629b4c091ad2ed36e
Abhijit Majumder <abhijit.majumder@gmail.com>
2020-01-25 22:05:46 -0500

    More updates to Matter
    changed the call condition to
    generate_vac_t_w_M to suppress excess warnings
    
    changed exit_condition to !exit_condition

--- examples/brickTest.cc
+++ examples/brickTest.cc
@@ -102,7 +102,7 @@ int main(int argc, char** argv)
 
 
   auto matter = make_shared<Matter> ();
-  auto lbt = make_shared<LBT> ();
+  //auto lbt = make_shared<LBT> ();
 
 
   // auto martini = make_shared<Martini> ();
