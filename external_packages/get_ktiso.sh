6fba77f8a78871c918d9dce2237f3d30c796d755
Derek Everett <everett@login3.stampede2.tacc.utexas.edu>
2020-03-18 09:45:34 -0500

    adding script to download ktiso code

---
+++ external_packages/get_ktiso.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Copyright (c) The JETSCAPE Collaboration, 2018
+#
+# For the list of contributors see AUTHORS.
+#
+# Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+#
+# or via email to bugs.jetscape@gmail.com
+#
+# Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+# See COPYING for details.
+##############################################################################
+
+# download the 2+1D OSU Kinetic Transport code
+git clone --depth=1 https://github.com/derekeverett/KTIso.git KTIso
