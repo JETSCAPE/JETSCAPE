e133c7dc663ac00d20e0120e89da0aba56a4cf8d
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-04-20 22:31:20 +0200

    New header v2 format option for final state outputs
    New header and output is off by default. To enable, pass anything
    in the third argument.
    
    Adds:
    - event weights to the header
    - consistent "\t" separation in the header
    - sigma gen and error to the end of file

--- examples/FinalStateHadrons.cc
+++ examples/FinalStateHadrons.cc
@@ -2,7 +2,7 @@
  * Copyright (c) The JETSCAPE Collaboration, 2018
  *
  * Modular, task-based framework for simulating all aspects of heavy-ion collisions
- * 
+ *
  * For the list of contributors see AUTHORS.
  *
  * Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
@@ -48,40 +48,83 @@ int main(int argc, char** argv)
   // //SetVerboseLevel (9 a lot of additional debug output ...)
   // //If you want to suppress it: use SetVerboseLevle(0) or max  SetVerboseLevel(9) or 10
   JetScapeLogger::Instance()->SetVerboseLevel(0);
-  
+
+  // Whether to write the new header (ie. v2), including xsec info.
+  // To enable, pass anything as the third argument to enable this option.
+  // Default: disabled.
+  bool writeHeaderV2 = false;
+  if (argc > 3) {
+    writeHeaderV2 = static_cast<bool>(argv[3]);
+    std::cout << "NOTE: Writing header v2, and final cross section and error at EOF.\n";
+  }
+
   auto reader=make_shared<JetScapeReaderAscii>(argv[1]);
   std::ofstream dist_output (argv[2]); //Format is SN, PID, E, Px, Py, Pz, Eta, Phi
   vector<shared_ptr<Hadron>> hadrons;
   int SN=0;
   while (!reader->Finished())
+  {
+    reader->Next();
+
+    // cout<<"Analyze current event: "<<reader->GetCurrentEvent()<<endl;
+
+    //dist_output<<"Event "<< reader->GetCurrentEvent()+1<<endl;
+    hadrons = reader->GetHadrons();
+    cout<<"Number of hadrons is: " << hadrons.size() << endl;
+
+    if (hadrons.size() > 0)
     {
-      reader->Next();
-      
-      // cout<<"Analyze current event: "<<reader->GetCurrentEvent()<<endl;
+      ++SN;
+      if (writeHeaderV2) {
+        // NOTE: Needs consistent "\t" between all entries to simplify parsing later.
+        dist_output << "#" << "\t"
+            << "Event\t" << SN << "\t"
+            << "weight\t" << reader->GetEventWeight() << "\t"
+            << "EPangle\t" << reader->GetEventPlaneAngle() <<"\t"
+            << "N_hadrons\t" << hadrons.size() << "\t"
+            << "|" << "\t"  // As a delimiter
+            << "N" << "\t"
+            << "pid" << "\t"
+            << "status" << "\t"
+            << "E"   << "\t"
+            << "Px"  << "\t"
+            << "Py"  << "\t"
+            << "Pz"  << "\t"
+            << "Eta" <<  "\t"
+            << "Phi" << "\n";
+      }
+      else {
+        dist_output << "#" << "\t"
+            << reader->GetEventPlaneAngle() << "\t"
+            << "Event"
+            << SN << "ID\t"
+            << hadrons.size() << "\t"
+            << "pstat-EPx"   << "\t"
+            << "Py"  << "\t"
+            << "Pz"  << "\t"
+            << "Eta" <<  "\t"<< "Phi" << "\n";
+      }
 
-      //dist_output<<"Event "<< reader->GetCurrentEvent()+1<<endl;
-      hadrons = reader->GetHadrons();
-      cout<<"Number of hadrons is: " << hadrons.size() << endl;
-
-      if(hadrons.size() > 0)
-	{
-	  SN++;
-	  dist_output << "#"<<"\t"  
-	              << reader->GetEventPlaneAngle() <<"\t"
-		      << "Event"
-		      << SN << "ID\t"
-		      << hadrons.size() << "\t"
-		      << "pstat-EPx"   << "\t"
-		      << "Py"  << "\t"
-		      << "Pz"  << "\t"
-		      << "Eta" <<  "\t"<< "Phi" << endl;
-	  
-	  for(unsigned int i=0; i<hadrons.size(); i++)
-	    {
-	      dist_output<<i<<" "<<hadrons[i].get()->pid()<<" "<<hadrons[i].get()->pstat()<<" "<< hadrons[i].get()->e() << " "<< hadrons[i].get()->px()<< " "<< hadrons[i].get()->py() << " "<< hadrons[i].get()->pz()<<  " " << hadrons[i].get()->eta() <<" "<<hadrons[i].get()->phi() <<endl;
-	    }
-	}
+      for (unsigned int i=0; i<hadrons.size(); i++)
+      {
+        dist_output << i
+            << " " << hadrons[i].get()->pid()
+            << " " << hadrons[i].get()->pstat()
+            << " " << hadrons[i].get()->e()
+            << " " << hadrons[i].get()->px()
+            << " " << hadrons[i].get()->py()
+            << " " << hadrons[i].get()->pz()
+            << " " << hadrons[i].get()->eta()
+            << " " << hadrons[i].get()->phi() << "\n";
+      }
     }
+  }
+  // Write the final cross section and error if requested by using header v2
+  if (writeHeaderV2) {
+    // NOTE: Needs consistent "\t" between all entries to simplify parsing later.
+    dist_output << "#" << "\t"
+        << "sigmaGen\t" << reader->GetSigmaGen() << "\t"
+        << "sigmaErr\t" << reader->GetSigmaErr() << "\n";
+  }
   reader->Close();
-  
 }
