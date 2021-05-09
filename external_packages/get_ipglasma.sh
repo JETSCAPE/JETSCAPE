561570bb3b800959f87844b7d56e92b66e72c881
Chun Shen <chunshen1987@gmail.com>
2021-05-09 11:59:23 -0400

    add a shell script to download the IPGlasma code and modified the CMake file

---
+++ external_packages/get_ipglasma.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Copyright (c) The JETSCAPE Collaboration, 2018
+#
+# For the list of contributors see AUTHORS.
+#
+# Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+#
+# or via email to bugs.jetscape@gmail.com
+#
+# Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+# See COPYING for details.
+##############################################################################
+
+# download the code package
+git clone --depth=1 https://github.com/chunshen1987/ipglasma ipglasma
+
+
