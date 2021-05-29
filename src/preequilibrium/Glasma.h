eb67548c05a245a89157ea123e8740459c2c9b51
Chun Shen <chunshen1987@gmail.com>
2021-05-29 14:31:49 -0400

    add Glasma pre-equilibrium module

---
+++ src/preequilibrium/Glasma.h
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) The JETSCAPE Collaboration, 2018
+ *
+ * Modular, task-based framework for simulating all aspects of heavy-ion collisions
+ * 
+ * For the list of contributors see AUTHORS.
+ *
+ * Report issues at https://github.com/JETSCAPE/JETSCAPE/issues
+ *
+ * or via email to bugs.jetscape@gmail.com
+ *
+ * Distributed under the GNU General Public License 3.0 (GPLv3 or later).
+ * See COPYING for details.
+ ******************************************************************************/
+
+#ifndef GLASMA_H
+#define GLASMA_H
+
+#include "PreequilibriumDynamics.h"
+
+using namespace Jetscape;
+
+class Glasma : public PreequilibriumDynamics {
+
+public:
+    Glasma();
+    ~Glasma() {};
+
+    void InitializePreequilibrium() {preequilibrium_status_ = INIT;};
+    void EvolvePreequilibrium();
+
+private:
+    // Allows the registration of the module so that it is available to be
+    // used by the Jetscape framework.
+    static RegisterJetScapeModule<Glasma> reg;
+};
+
+#endif   // GLASMA_H
