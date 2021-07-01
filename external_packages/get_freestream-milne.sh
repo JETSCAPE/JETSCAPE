64d61bbf275b3c6353e56e38d67b2178d9ac9e77
Chun Shen <chunshen1987@gmail.com>
2021-07-01 12:07:16 -0400

    update the input files for freestream and music modules for output the evolution history

--- external_packages/get_freestream-milne.sh
+++ external_packages/get_freestream-milne.sh
@@ -14,6 +14,8 @@
 ##############################################################################
 
 # download the 3+1D OSU freestreaming code
-git clone --depth=1 https://github.com/derekeverett/freestream-milne.git freestream-milne
+#git clone --depth=1 https://github.com/derekeverett/freestream-milne.git freestream-milne
+#git clone --depth=1 https://github.com/derekeverett/freestream-milne.git -b time_step_history freestream-milne
+git clone --depth=1 https://github.com/chunshen1987/freestream-milne -b time_step_history freestream-milne
 #cd freestream-milne
 #git checkout double_prec
