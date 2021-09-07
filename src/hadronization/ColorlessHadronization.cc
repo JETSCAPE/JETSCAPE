991f9110b6763daf964dbdc984bd9e923542138f
Amit Kumar <amitkr2410>
2021-09-06 23:36:08 -0400

    Read strings from XML for hadron decay setting, add HadronPartonWriter tags in master XML file

--- src/hadronization/ColorlessHadronization.cc
+++ src/hadronization/ColorlessHadronization.cc
@@ -89,6 +89,16 @@ void ColorlessHadronization::Init() {
     pythia.readString("ParticleDecays:tau0Max = 10.0");
   }
   
+  std::stringstream lines;
+  lines << GetXMLElementText({"JetHadronization", "LinesToRead"}, false);
+  int i = 0;
+  while (std::getline(lines, s, '\n')) {
+    if (s.find_first_not_of(" \t\v\f\r") == s.npos)
+      continue; // skip empty lines
+    JSINFO << "Also reading in: " << s;
+    pythia.readString(s);
+  }
+
   // Initialize random number distribution
   ZeroOneDistribution = std::uniform_real_distribution<double> { 0.0, 1.0 };
   // And initialize
