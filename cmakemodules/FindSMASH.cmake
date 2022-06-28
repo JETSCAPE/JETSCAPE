9a36c2840e4ab6ee24129a11ab739b85e27c4480
Jan Staudenmaier <staudenmaier@fias.uni-frankfurt.de>
2022-06-28 12:43:26 +0200

    Update FindSMASH cmake module for new SMASH version

--- cmakemodules/FindSMASH.cmake
+++ cmakemodules/FindSMASH.cmake
@@ -9,12 +9,23 @@
 # The environment variable SMASH_DIR must be set properly to succeed, e. g.:
 # export SMASH_DIR=~/Work/SMASH/smash
 
+# At the moment a macro about the system endianness is needed from within SMASH
+include(TestBigEndian)
+TEST_BIG_ENDIAN(IS_BIG_ENDIAN)
+if(IS_BIG_ENDIAN)
+   message(STATUS "Big endian architecture detected.")
+   add_definitions("-DBIG_ENDIAN_ARCHITECTURE")
+else()
+   message(STATUS "Little endian architecture detected.")
+   add_definitions("-DLITTLE_ENDIAN_ARCHITECTURE")
+endif()
+
 message(STATUS "Looking for SMASH ...")
 
 set(SMASH_INCLUDE_DIR
-   $ENV{SMASH_DIR}/3rdparty/Cuba-4.2
+   $ENV{SMASH_DIR}/3rdparty/Cuba-4.2.1
    $ENV{SMASH_DIR}/3rdparty/einhard
-   $ENV{SMASH_DIR}/3rdparty/yaml-cpp-0.6.2/include
+   $ENV{SMASH_DIR}/3rdparty/yaml-cpp-0.7.0/include
    $ENV{SMASH_DIR}/build/src/include
    $ENV{SMASH_DIR}/src/include
 )
@@ -22,8 +33,8 @@ message(STATUS "SMASH includes found in ${SMASH_INCLUDE_DIR}")
 
 find_library(SMASH_LIBRARY NAMES smash PATHS $ENV{SMASH_DIR}/build/src)
 find_library(EINHARD_LIBRARY NAMES einhard PATHS $ENV{SMASH_DIR}/build/3rdparty/einhard)
-find_library(CPPYAML_LIBRARY NAMES yaml-cpp PATHS $ENV{SMASH_DIR}/build/3rdparty/yaml-cpp-0.6.2)
-find_library(INTEGRATION_LIBRARY NAMES cuhre PATHS $ENV{SMASH_DIR}/build/3rdparty/Cuba-4.2/src/cuhre)
+find_library(CPPYAML_LIBRARY NAMES yaml-cpp PATHS $ENV{SMASH_DIR}/build/3rdparty/yaml-cpp-0.7.0)
+find_library(INTEGRATION_LIBRARY NAMES cuhre PATHS $ENV{SMASH_DIR}/build/3rdparty/Cuba-4.2.1/src/cuhre)
 set(SMASH_LIBRARIES ${EINHARD_LIBRARY} ${CPPYAML_LIBRARY} ${SMASH_PYTHIA_LIBRARY} ${SMASH_LIBRARY} ${INTEGRATION_LIBRARY})
 
 message(STATUS "SMASH libraries: ${SMASH_LIBRARIES}")
