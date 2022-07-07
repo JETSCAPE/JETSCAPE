d2fffbadab1c69e32689a9e6231624f03530c90f
Joe Latessa <jlatessa@gmail.com>
2022-07-07 13:29:48 -0400

    added exception handling to load_site script

--- test/plot/load_site.py
+++ test/plot/load_site.py
@@ -2,7 +2,15 @@
 
 import requests as req
 
-resp = req.request(method='GET', url="http://plot.joelatessa.com")
+# web-site's file processing is done onload().  This script
+# accesses the site, so any .gz file uploaded from a workflows-
+# run is processed before a new file is uploaded.
+
+try:
+    resp = req.request(method='GET', url="http://plot.joelatessa.com")
+except Exception:
+    print('could not access site... continuing')
+    pass
 
 # show in terminal for testing
 # print(resp.text)
