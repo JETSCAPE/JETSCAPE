6b68dd94838229d232d2f519b6194a59da4c1f07
Loren Schwiebert <loren@wayne.edu>
2022-07-30 11:55:54 -0400

    Change from XML Master file to XML Main file

--- examples/custom_examples/hydroJetTestPGun.cc
+++ examples/custom_examples/hydroJetTestPGun.cc
@@ -84,7 +84,7 @@ int main(int argc, char** argv)
   Show();
 
   auto jetscape = make_shared<JetScape>();
-  jetscape->SetXMLMasterFileName("../config/jetscape_master.xml");
+  jetscape->SetXMLMainFileName("../config/jetscape_main.xml");
   jetscape->SetXMLUserFileName("../config/jetscape_user.xml");
   // auto jetscape = make_shared<JetScape>("./jetscape_init_pythiagun.xml",5);
   jetscape->SetId("primary");
