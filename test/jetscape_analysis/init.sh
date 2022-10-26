bb1fe9224d6dbf71254c3ee133513bb74617a38e
Joe Latessa <jlatessa@gmail.com>
2022-10-26 01:23:13 -0400

    removing test folder which is now included in the TEST-EXAMPLES repo

--- test/jetscape_analysis/init.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# This script should be run to initialize the analysis environment
-# if you are using the JETSCAPE docker container.
-
-# Load heppy module
-source /usr/local/init/profile.sh
-module load heppy/1.0
