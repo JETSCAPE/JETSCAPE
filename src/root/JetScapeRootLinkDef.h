59399453a7b3e1591c6de613a139b2e125804139
Joern Putschke <joern.putschke@wayne.edu>
2022-12-14 12:50:49 -0500

    Remove dict generation for ROOT (HepMC)
    Dictionary not needed for HepMC ROOT writer ...

--- src/root/JetScapeRootLinkDef.h
+++ src/root/JetScapeRootLinkDef.h
@@ -5,15 +5,9 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
-#pragma link C++ class JetScapeRootParton+;
-#pragma link C++ class JetScapeRootPartonShower+;
-#pragma link C++ class JetScapeRootVertex+;
-#pragma link C++ class JetScapeRootEvent+;
-#pragma link C++ class JetScapeWriterRoot+;
-#pragma link C++ class JetScapeWriterRootHepMC+;
+#pragma link C++ class Jetscape::JetScapeWriterRootHepMC+;
 
 #pragma link C++ class Jetscape::PartonShower+;
-#pragma link C++ class Jetscape::PartonShowerPy8+;
 #pragma link C++ class Jetscape::Vertex+;
 #pragma link C++ class Jetscape::Parton+;
 #pragma link C++ class Jetscape::Hadron+;
