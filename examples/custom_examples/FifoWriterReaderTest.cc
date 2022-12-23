8077c7cbc28b68c96fe1402d543c2af9eb26c78f
hmehryar <haydarmehryar@gmail.com>
2022-12-23 21:55:27 +0000

    adding a sample fifo reader-writer for checking fixes #142

---
+++ examples/custom_examples/FifoWriterReaderTest.cc
@@ -0,0 +1,48 @@
+
+// C program to implement one side of FIFO
+// This side writes first, then reads
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd;
+
+    // FIFO file path
+    char * myfifo = "/tmp/myfifo";
+
+    // Creating the named file(FIFO)
+    // mkfifo(<pathname>, <permission>)
+    mkfifo(myfifo, 0666);
+
+    char arr1[80], arr2[80];
+    while (1)
+    {
+        // Open FIFO for write only
+        fd = open(myfifo, O_WRONLY);
+
+        // Take an input arr2ing from user.
+        // 80 is maximum length
+        fgets(arr2, 80, stdin);
+
+        // Write the input arr2ing on FIFO
+        // and close it
+        write(fd, arr2, strlen(arr2)+1);
+        close(fd);
+
+        // Open FIFO for Read only
+        fd = open(myfifo, O_RDONLY);
+
+        // Read from FIFO
+        read(fd, arr1, sizeof(arr1));
+
+        // Print the read message
+        printf("User2: %s\n", arr1);
+        close(fd);
+    }
+    return 0;
+}
