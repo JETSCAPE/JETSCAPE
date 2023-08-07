1d3091a65bd172ffeba3f22490490fac15673926
Cameron Parker <cameron.parker@tamu.edu>
2023-08-07 18:03:03 -0500

    pandas design compatibility

--- functions.py
+++ functions.py
@@ -3,8 +3,9 @@ import sys
 from pyDOE import lhs 
 import numpy as np
 import random
+import pandas as pd
 
-#gets list of directories in the target directory
+# gets list of directories in the target directory
 def getDirs(dir):
     startdir = os.getcwd()
     os.chdir(dir)
@@ -17,10 +18,10 @@ def getDirs(dir):
     os.chdir(startdir)
 
     
-    directories.sort()  #to make sure the directories are sorted in case it doesnt complete
+    directories.sort()  # to make sure the directories are sorted in case it doesnt complete
     return directories
 
-#to see what ran last if runs dont complete
+# to see what ran last if runs dont complete
 def update(cmd):
     outfile = open('last-ran.txt','a')
     outfile.write(cmd+'\n')
@@ -28,21 +29,21 @@ def update(cmd):
 
     print(cmd)
 
-#reading design file from an existing run
+# reading design file from an existing run
 def readDesign(designfile):
     design = []
 
-    #opening file
+    # opening file
     file = open(designfile, 'r')
     lines = file.readlines()
 
     index = 0
     for line in lines:
-        #skipping comments
+        # kipping comments
         if '#' in line:
             continue
         
-        #splitting line and converting to floats
+        # splitting line and converting to floats
         params = line.split()
         newparams = [index]
         for param in params:
@@ -52,74 +53,98 @@ def readDesign(designfile):
 
     return design
 
-##ranges of form [(start,end),(start,end)]
-def createDesign(points):
-    ##initialization
-    rawdesign = lhs(8, samples = points)
+# ranges of form [(start,end),(start,end)]
+def createDesign(points, designparams):
+    # initialization
+    rawdesign = lhs(len(designparams), samples = points)
     index = 0
     design = []
-    print(rawdesign)
 
-    #scaling to param ranges
-    for Q, vir, lQCD, pionWidth, kaonWidth, protonWidth, StoUD, QQtoQ in rawdesign:
-        ##Rescaling LHS parameters to appropriate ranges
-        Qnew = (Q*2.1 + 0.9)
-        virnew = (vir*.9 + .1)
-        lQCDnew = (lQCD*0.3 + 0.1)
-        pionWidthnew = (pionWidth*1.5 + 0.5)
-        kaonWidthnew = (kaonWidth*1.5 + 0.5)
-        protonWidthnew = (protonWidth*1.5 + 0.5)
-        StoUDnew = (StoUD*0.3+0.2)
-        QQtoQnew = (QQtoQ*0.13+0.07)
-        design.append([index, Qnew, virnew, lQCDnew, pionWidthnew, kaonWidthnew, protonWidthnew, StoUDnew, QQtoQnew])
+    # Rescaling LHS parameters to appropriate ranges
+    for point in rawdesign:
+        newpoint = []
+        newpoint.append({"index": index})
+        for i, param in enumerate(designparams):
+            newpoint.append({param["name"]: (point[i]*(param["range"][1]-param["range"][0]) + param["range"][0])})
+
+        design.append(newpoint)
         index = index + 1
 
     return design
 
-##augment latin hypercube
+# ranges of form [(start,end),(start,end)]
+def createPandaDesign(points):
+    # design parameters
+    designparams = [
+        {"name": "Q0", "range": (0.95,3.0)},
+        {"name": "vir_factor", "range": (0.1,1.0)},
+        {"name": "lambdaQCD", "range": (0.1,0.4)},
+        {"name": "shower_recofactor", "range": (0.5,2.0)},
+        {"name": "StringFlav:probStoUD", "range": (0.2,0.5)},
+        {"name": "StringFlav:probQQtoQ", "range": (0.07,0.2)},
+        {"name": "MultipartonInteractions:ecmPow", "range": (0.0,0.25)},
+        {"name": "MultipartonInteractions:pT0Ref", "range": (0.5,2.5)},
+    ]
+
+    # initialization
+    rawdesign = lhs(len(designparams), samples = points)
+    design = []
+
+    # Rescaling LHS parameters to appropriate ranges
+    for point in rawdesign:
+        newpoint = []
+        for i, param in enumerate(designparams):
+            newpoint.append((point[i]*(param["range"][1]-param["range"][0]) + param["range"][0]))
+
+        design.append(newpoint)
+
+    df = pd.DataFrame(design,columns=[designparam["name"] for designparam in designparams])
+    return df
+
+# augment latin hypercube
 def augmentDesign(points, bounds, n):
     m = len(points)
     nparams = len(points[0])-1
     temppoints = points
 
-    #setting up intervals for hypercube cells
+    # setting up intervals for hypercube cells
     intervals = []
-    for bound in bounds:    #loop over parameters
+    for bound in bounds:    # loop over parameters
         thisinterval = []
         for i in range(m+n):
             thisinterval.append(bound[0]+(i*(bound[1]-bound[0])/(m+n)))
         thisinterval.append(bound[1])
         intervals.append(thisinterval)
 
-    #creating cube of cells with true and false values
+    # creating cube of cells with true and false values
     cells = np.full((m+n, nparams), False)
-    for i, row in enumerate(cells):     #loop over rows
-        for j, cell in enumerate(row):  #loop over columns
-            for point in temppoints:    #loop over points to match (shifted by 1 to skip index)
+    for i, row in enumerate(cells):     # loop over rows
+        for j, cell in enumerate(row):  # loop over columns
+            for point in temppoints:    # loop over points to match (shifted by 1 to skip index)
                 if point[j+1] >= intervals[j][i] and point[j+1] < intervals[j][i+1]:
                     cells[i][j] = True
 
-    #getting list of cells that are still empty
+    # getting list of cells that are still empty
     emptyCellIDs = []
-    for i in range(len(cells[0])):      #loop over parameters
+    for i in range(len(cells[0])):      # loop over parameters
         tempCellIDs = []
-        for j in range(len(cells)):     #loop over cells for each parameter
+        for j in range(len(cells)):     # loop over cells for each parameter
             if cells[j][i] == False:
                 tempCellIDs.append(j)
         emptyCellIDs.append(tempCellIDs)        
 
-    #creating points to fill holes
+    # creating points to fill holes
     newpoints = []
     index = 0
-    for i in range(n):              #loop for every point to add
+    for i in range(n):              # loop for every point to add
         newparams = [index]
-        for j in range(nparams):    #loop over each parameter
-            #getting a random cell ID to pull parameter value from
+        for j in range(nparams):    # loop over each parameter
+            # getting a random cell ID to pull parameter value from
             r = random.randint(0,len(emptyCellIDs[j])-1)
             cellID = emptyCellIDs[j][r]
             emptyCellIDs[j].remove(cellID)
 
-            #picking randomly in that cells range
+            # picking randomly in that cells range
             newparam = (random.random()*(intervals[j][cellID+1]-intervals[j][cellID]))+intervals[j][cellID]
             newparams.append(newparam)
 
@@ -128,17 +153,27 @@ def augmentDesign(points, bounds, n):
 
     return newpoints
 
-#write the the design out with the header provided
-def writeDesign(points, header, filename):
-    newqvirlines = [header]
-    for index, Q, vir, lqcd, pionWidth, kaonWidth, protonWidth, StoUD, QQtoQ in points:
-        newqvirlines.append(str(Q)+" "+str(vir)+" "+str(lqcd)+" "+str(pionWidth)+" "+str(kaonWidth)+" "+str(protonWidth)+" "+str(StoUD)+" "+str(QQtoQ)+"\n")
+# write the the design out with the header provided
+def writeDesign(points, ECM, filename):
+    header = "#"
+    keysList = [i for s in [d.keys() for d in points[0]] for i in s]
+    keysList = keysList[1:] # removing index column
+    for key in keysList:
+        header = header + " " + key
+
+    newqvirlines = ["# "+ECM+"\n","# Version 1.0\n",header+"\n"]
+    for point in points:
+        line = ""
+        for i, param in enumerate(point[1:]):
+            line = line + str(param.get(keysList[i])) + " "
+    
+        newqvirlines.append(line+"\n")
 
     qvirCode = open(filename,'w')
     qvirCode.writelines(newqvirlines)
     qvirCode.close()
 
-#runs the xml in jetscape
+# runs the xml in jetscape
 def runxml(xmlname):
     runjetscapecmd = "./runJetscape " + xmlname
     update(runjetscapecmd)
--- make-pp-bins.py
+++ make-pp-bins.py
@@ -1,5 +1,4 @@
 #makes set of bayesian design points for pp collisions
-#first argument is Ecm, with 2760 being treated as a primary run
 
 import os
 import multiprocessing as mp
@@ -7,10 +6,11 @@ from pickle import TRUE
 from pyDOE import lhs 
 from datetime import date
 from functions import *
+import pandas as pd
 from operator import itemgetter
 import time
 
-##option reading
+# option reading
 system = "LHC"
 RHIC = False
 Drun = False
@@ -40,7 +40,7 @@ for i, option in enumerate(sys.argv):
         softOnly = True
     if "-d" in option:
         reading = True
-        design = readDesign(sys.argv[i+1])
+        design = pd.read_csv(sys.argv[i+1])
     if "-r" in option:
         rerunning = True
         reading = True
@@ -51,18 +51,18 @@ for i, option in enumerate(sys.argv):
         reading = True
         startdir = sys.argv[i+1]
 
-#date and file initiation
+# date and file initiation
 todays_date = date.today()
 totaldir = "/scratch/user/cameron.parker/newJETSCAPE/JETSCAPE/runs/" + system + "-" + str(todays_date.month) + "-" + str(todays_date.day) + "/"
 if rerunning: 
     totaldir = startdir
-    design = readDesign(totaldir+"QVir_Analysis/parameters.txt")
+    design = pd.read_csv(totaldir+"QVir_Analysis/parameters.txt")
     print("Using: "+totaldir+"QVir_Analysis/parameters.txt")
 
-
+# function to make xmls
 def makexml(bound, parameters, baseDir, xmltemplate, ECM):
     events  = 20000
-    #extra events in first bin
+    # extra events in first bin
     if RHIC:
         events = events*6
     if Drun:
@@ -74,45 +74,52 @@ def makexml(bound, parameters, baseDir, xmltemplate, ECM):
     if bound[0] == 0:
         events = events*2
 
-    #appending option and filename
+    # appending option and filename
     ogfile = baseDir + "/dat/PP_Bin" + str(bound[0])+ "_" + str(bound[1])
     if appending:
         filename = ogfile+"temp"
     else:
         filename = ogfile
 
-    #building lines for xml file
+    # building lines for xml file
     eventLine = "  <nEvents> " + str(events) + " </nEvents>"
     lowerLine = "      <pTHatMin>" + str(bound[0]) + "</pTHatMin>\n"
     upperLine = "      <pTHatMax>" + str(bound[1]) + "</pTHatMax>\n"
     fileLine = "  <outputFilename>" + filename + "</outputFilename>\n"
-    qLine = "      <Q0>" + str(parameters[1]) + "</Q0>\n"
-    virLine = "      <vir_factor>" + str(parameters[2]) + "</vir_factor>\n"
-    lambdaline = "    <lambdaQCD> " + str(parameters[3]) + " </lambdaQCD>"
-    pionLine = "    <pionWidthScale>" + str(parameters[4]) + "</pionWidthScale>\n"
-    kaonLine = "    <kaonWidthScale>" + str(parameters[5]) + "</kaonWidthScale>\n"
-    protonLine = "    <protonWidthScale>" + str(parameters[6]) + "</protonWidthScale>\n"
-    StoUDline = "      StringFlav:probStoUD = " + str(parameters[7]) + "\n"
-    QQtoQline = "      StringFlav:probQQtoQ = " + str(parameters[8]) + "\n"
-    pt0refline = "         MultipartonInteractions:pT0Ref = " + str(parameters[9]) +"\n"
-
+    
+    paramlines = []
+    for key in parameters.columns:
+        paramline = {}
+        paramline["name"] = key
+        if ":" in key:
+            paramline["line"] = "\t\t\t"+key+" = "+str(parameters.iloc[-1][key])+"\n"
+        else:
+            paramline["line"] = "\t\t\t<"+key+">"+str(parameters.iloc[-1][key])+"</"+key+">\n"
+        paramlines.append(paramline)
 
     newlines = []
     for line in xmltemplate:
-        if "<nEvents>" in line: newlines.append(eventLine)
-        elif "<pTHatMin>" in line: newlines.append(lowerLine)
-        elif "<pTHatMax>" in line: newlines.append(upperLine)
-        elif "<outputFilename>" in line: newlines.append(fileLine)
-        elif "<Q0>" in line: newlines.append(qLine)
-        elif "<vir_factor>" in line: newlines.append(virLine)
-        elif "<lambdaQCD>" in line: newlines.append(lambdaline)
-        elif "<pionWidthScale>" in line: newlines.append(pionLine)
-        elif "<kaonWidthScale>" in line: newlines.append(kaonLine)
-        elif "<protonWidthScale>" in line: newlines.append(protonLine)
-        elif "StoUD" in line: newlines.append(StoUDline)
-        elif "QQtoQ" in line: newlines.append(QQtoQline)
-        elif "pT0Ref" in line: newlines.append(pt0refline)
-        else: newlines.append(line)
+        if "<nEvents>" in line: 
+            newlines.append(eventLine)
+            continue
+        elif "<pTHatMin>" in line:
+            newlines.append(lowerLine)
+            continue
+        elif "<pTHatMax>" in line:
+            newlines.append(upperLine)
+            continue
+        elif "<outputFilename>" in line:
+            newlines.append(fileLine)
+            continue
+
+        writeold = True
+        for paramline in paramlines:
+            if paramline["name"] in line: 
+                newlines.append(paramline["line"])
+                writeold = False
+                break
+
+        if writeold: newlines.append(line)
 
     xmlname = baseDir+"/xml/pp_"+ECM+"_"+str(bound[0])+"_"+str(bound[1])+".xml"
     xml = open(xmlname,'w')
@@ -122,9 +129,9 @@ def makexml(bound, parameters, baseDir, xmltemplate, ECM):
     output = {'xml': xmlname, 'dat': ogfile}
     return output
 
-#making directory for the runs
+# making directory for the runs
 def makeDir(index):
-    #setting names
+    # setting names
     baseDir = totaldir + str(index)
     plotsDir = baseDir + "/plots"
     rootDir = baseDir + "/root"
@@ -155,10 +162,10 @@ def makeDir(index):
     #returning base directory name
     return baseDir
 
-####################################################################################################################
-##Start of main method
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Start of main method
 
-##Creating total directory for use
+# Creating total directory for use
 try:
     os.makedirs(totaldir)
 except:
@@ -169,17 +176,17 @@ try:
 except:
     pass
 
-##pTHat bounds and xml based on RHIC vs LHC
+# pTHat bounds and xml based on RHIC vs LHC
 intervals = []
 ECM = ""
 if RHIC:
     intervals = [0, 5, 7.5, 10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30, 32.5, 35, 37.5, 40, 42.5, 45, 50, 55, 60, 65, 70]
     ECM = "200"
 elif Drun or Erun:
-    intervals = range(0,48+1) #need to add one more to the range than bins desired
+    intervals = range(0,48+1) # need to add one more to the range than bins desired
     ECM = "5020"
 elif Lrun:
-    intervals = range(0,48+1) #need to add one more to the range than bins desired
+    intervals = range(0,48+1) # need to add one more to the range than bins desired
     ECM = "13000"
 else:
     intervals = [0, 5, 7.5, 10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30, 32.5, 35, 37.5, 40, 42.5, 45, 50, 55, 60, 65, 70, 75, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 230, 250, 270, 290, 310, 330, 350, 400, 450, 500, 550, 600, 1000]
@@ -190,54 +197,46 @@ xmlname = "/scratch/user/cameron.parker/newJETSCAPE/JETSCAPE/config/jetscape_use
 xmltemplate = open(xmlname,"r")
 xmllines = xmltemplate.readlines()
 
-##design points
+# making design points
 nsamples = 48
-if not reading: design = createDesign(nsamples)
+if not reading: design = createPandaDesign(nsamples)
 
-#pthat bounds formed into pairs
+# pthat bounds formed into pairs
 pTHatBounds = []
 for i in range(len(intervals)-1):
     if softOnly:
-        pTHatBounds.append((0,-i-1)) #setting all soft bins upper bound to negative
+        pTHatBounds.append((0,-i-1)) # setting all soft bins upper bound to negative
     elif i is 0 and intervals[i] == 0:
         pTHatBounds.append((intervals[i],-1))
     else:
         pTHatBounds.append((intervals[i],intervals[i+1]))
 
-##Changing to build directory
+# Changing to build directory
 os.chdir("/scratch/user/cameron.parker/newJETSCAPE/JETSCAPE/build")
 
 pool = mp.Pool(len(pTHatBounds))
 
-##Loop over design points and make parameter file
-newqvirlines = ["# "+ECM+"\n","# Version 1.0\n", "# Parameter q0 virFac lambdaQCD pionScale kaonScale protonScale StoUD QQtoQ pT0Ref\n"]
-for index, Q, vir, lqcd, pionWidth, kaonWidth, protonWidth, StoUD, QQtoQ, pt0ref in design:
-    newqvirlines.append(str(Q)+" "+str(vir)+" "+str(lqcd)+" "+str(pionWidth)+" "+str(kaonWidth)+" "+str(protonWidth)+" "+str(StoUD)+" "+str(QQtoQ)+" "+str(pt0ref)+"\n")
+# Loop over design points and make parameter file
+design.to_csv(totaldir+'QVir_Analysis/parameters.txt')
+print(design)
 
-qvirCode = open(totaldir + 'QVir_Analysis/parameters.txt','w')
-qvirCode.writelines(newqvirlines)
-qvirCode.close()
+for i in range(len(design)):    
+    # Making xmls
+    baseDir = makeDir(i)
+    output = pool.starmap(makexml, [(bound, design.loc[[i]], baseDir, xmllines, ECM) for bound in pTHatBounds]) # star map to each set of bounds
 
-for index, Q, vir, lQCD, pionWidth, kaonWidth, protonWidth, StoUD, QQtoQ, pt0ref in design:
-    parameters = [index, Q, vir, lQCD, pionWidth, kaonWidth, protonWidth, StoUD, QQtoQ, pt0ref]
-    print(parameters)
-
-    ##Making xmls
-    baseDir = makeDir(parameters[0])
-    output = pool.starmap(makexml, [(bound, parameters, baseDir, xmllines, ECM) for bound in pTHatBounds]) #star map to each set of bounds
-
-    ##Running jetscape for them
+    # Running jetscape for them
     xmls, dats = zip(*map(itemgetter('xml', 'dat'), output)) 
-    pool.map(runxml, xmls)
+    #pool.map(runxml, xmls)
 
-    ##Handling appending runs
+    # Handling appending runs
     if appending:
         for dat in dats:
             appendcommand = "cat " + dat + "temp.dat >> " + dat + ".dat"
             os.system(appendcommand)
             os.system('rm '+dat+'temp.dat')
 
-    ##concatonating all soft bins together
+    # concatonating all soft bins together
     if softOnly:
         cmd = "cat "
         for dat in dats:
