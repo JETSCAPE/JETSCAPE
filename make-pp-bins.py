57147c3b2e2c4abfa98f803479a7ac4ce13f92b4
Cameron Parker <cameron.parker@tamu.edu>
2023-08-29 18:01:35 -0500

    updating event counts

--- make-pp-bins.py
+++ make-pp-bins.py
@@ -79,12 +79,10 @@ if rerunning:
 
 # function to make xmls
 def makexml(bound, parameters, baseDir, xmltemplate, ECM):
-    events  = 20000
+    events  = 10000
     # extra events in first bin
     if RHIC:
         events = events*6
-    if Erun:
-        events = 100000
     if bound[0] == 0:
         events = events*2
 
