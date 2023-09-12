302283feb321a3c863068b2163f7d5f0c22a679b
Cameron Parker <cameron.parker@tamu.edu>
2023-09-12 14:38:39 -0500

    updating charm test for epem

--- execs/ee-charm-spectra.cc
+++ execs/ee-charm-spectra.cc
@@ -55,6 +55,7 @@
 #include "TMultiGraph.h"
 #include "TLegend.h"
 #include "TRatioPlot.h"
+#include "THStack.h"
 
 //my library
 #include "analysis.h"
@@ -76,12 +77,20 @@ int main(int argc, char* argv[]){
     //System variables
     double Ecm = 91.2;
     
-    //Variables for single hadron spectrum and identified hadrons
-    double SingleHadronpTBin[] = {0.004,0.006,0.008,0.01,0.012,0.014,0.016,0.018,0.02,0.025,0.03,0.035,0.04,0.045,0.05,0.06,0.07,0.08,0.09,0.1,0.11,0.12,0.13,0.14,0.16,0.18,0.2,0.225,0.25,0.275,0.3,0.325,0.35,0.375,0.4,0.43,0.46,0.49,0.52,0.55,0.6,0.65,0.7,0.75,0.8,0.9,1};
+    //Variables for single hadron spectrum and identified hadrons from https://arxiv.org/pdf/hep-ex/9909032.pdf
+    double SingleHadronpTBin[] = {0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0};
+    double spectrumData[] = {7.47,9.03,10.42,10.76,9.89,8.97,8.17,6.94,6.73,5.56,4.94,3.49,3.13,2.00,1.27,0.50,0.27,0.06};
+    double spectrumError[] = {0.63,0.49,0.44,0.43,0.38,0.35,0.32,0.28,0.27,0.24,0.22,0.18,0.17,0.14,0.11,0.07,0.05,0.03};
     int NpTSingleHadronBin = sizeof(SingleHadronpTBin)/sizeof(SingleHadronpTBin[0])-1;
+    double spectrumX[NpTSingleHadronBin], spectrumWidth[NpTSingleHadronBin];
+    for(int i=0; i<NpTSingleHadronBin; i++){
+        spectrumX[i] = (SingleHadronpTBin[i+1]+SingleHadronpTBin[i])/2;
+        spectrumWidth[i] = (SingleHadronpTBin[i+1]-SingleHadronpTBin[i])/2;
+    }
 
     //reco hadrons
-    TH1D *HistRecoHadron = new TH1D("Frag Hadrons", "Frag Hadrons", NpTSingleHadronBin, SingleHadronpTBin);
+    TH1D *HistDstars = new TH1D("Frag Hadrons", "Frag Hadrons", NpTSingleHadronBin, SingleHadronpTBin);
+    TGraphErrors *data = new TGraphErrors(NpTSingleHadronBin, spectrumX, spectrumData, spectrumWidth, spectrumError);
     
     //particle info declaration
     int  SN=0,PID=0;
@@ -93,12 +102,12 @@ int main(int argc, char* argv[]){
     // Create a file on which histogram(s) can be saved.
     char outFileName[1000];
     sprintf(outFileName,"SpectraBin.root");
-    TFile* outFile = new TFile( outFileName, "RECREATE");
+    TFile* outFile = new TFile(outFileName, "RECREATE");
     
     //opening run file
     char HadronFile[300], pTBinString[100];
-    sprintf(HadronFile,"run.dat");
-    auto myfile  = make_shared<JetScapeReaderAscii>(HadronFile);
+    sprintf(HadronFile,"run.dat.gz");
+    auto myfile  = make_shared<JetScapeReaderAsciiGZ>(HadronFile);
        
     //event loop
     while (!myfile->Finished()){
@@ -121,25 +130,19 @@ int main(int argc, char* argv[]){
             double P = TMath::Sqrt((Px*Px) + (Py*Py) + (Pz*Pz));
 
             //Add to multiplicity and prepare for xp filling
-            if(PT > 0.2 && abs(Eta) < 1.74 && (fabs(PID) > 100 || abs(PID) == 11 || abs(PID) == 13 || abs(PID) == 15) && pythia.particleData.charge(PID)!= 0){
+            if(abs(PID) == 413){
                 //multiplicity++;
-                pts.push_back(P);
-                ids.push_back(PID);
+                double xe = 2*E/Ecm;
+                HistDstars->Fill(xe);
             }
-        
-
-        
         }
     }
     myfile->Close();
-
-    //Write unprocessed data into a root file
-    HistTempSingleHadron->Write();
     
     //scaling histograms
-    HistMultiplicity->Scale(1/(double)Events); //times 2 to match the data
-    HistTempSingleHadron->Scale(1/(double)Events,"width");
-    HistRecoHadron->Scale(1/(double)Events,"width");
+    HistDstars->Scale(1/(double)Events,"width");
+    HistDstars->Write("Reco Hadron Ratio");
+    ratioPlot(data,HistDstars,"D* xp");
     
     TVector EventInfo(3);
     EventInfo[0] = 1;
@@ -148,16 +151,6 @@ int main(int argc, char* argv[]){
     EventInfo.Write("EventInfo");
     outFile->Close();
 
-    //ee Jets debugging
-    //cout << integral(HistTempJet) << endl;
-
-    //create root file for total plots
-    TFile* totalroot = new TFile("totals.root", "RECREATE");
-    HistTempSingleHadron->Write("hadrons");
-    HistRecoHadron->Write("Reco Hadron Ratio");
-
-    //ratio plots for comparison
-    TGraphErrors* thrustgraph = (TGraphErrors*) thrustdir->Get("Graph1D_y1");
 
     //Done. Script run time
     int EndTime = time(NULL);
