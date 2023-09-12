fd57e024b57a77be9278f47334d037926447d1c9
Cameron Parker <cameron.parker@tamu.edu>
2023-09-12 14:29:00 -0500

    updating epem maker

--- make-ee-bins.py
+++ make-ee-bins.py
@@ -2,13 +2,29 @@
 
 import os
 import multiprocessing as mp
+import pandas as pd
 from pyDOE import * 
 from datetime import date
 from functions import *
 
+#option reading
+for i, option in enumerate(sys.argv):
+    if "-d" in option:
+        reading = True
+        design = pd.read_csv(sys.argv[i+1])
+    if "-r" in option:
+        rerunning = True
+        reading = True
+        startdir = sys.argv[i+1]
+    if "-a" in option:
+        appending = True
+        rerunning = True
+        reading = True
+        startdir = sys.argv[i+1]
+
 #date and file initiation
 todays_date = date.today()
-totaldir = "/scratch/user/cameron.parker/JETSCAPE-COMP-HH_colorrecomb/runs/LEP-" + str(todays_date.month) + "-" + str(todays_date.day) + "/"
+totaldir = "/scratch/user/cameron.parker/newJETSCAPE/JETSCAPE/runs/LEP-" + str(todays_date.month) + "-" + str(todays_date.day) + "/"
 
 try:
     os.makedirs(totaldir)
@@ -21,33 +37,36 @@ except:
     pass
 
 #methods for runnning
-def binrun(designpoint, xmltemplate):
+def binrun(index, parameters, xmltemplate):
     ##making directory for this designpoint
-    baseDir = makeDir(designpoint[0])
+    baseDir = makeDir(index)
 
     ##building lines for xml file
     fileLine = "    <outputFilename>" + baseDir + "/run" + "</outputFilename>\n"
-    qLine = "            <Q0>" + str(designpoint[1]) + "</Q0>\n"
-    virLine = "            <vir_factor>" + str(designpoint[2]) + "</vir_factor>\n"
-    lambdaline = "        <lambdaQCD> " + str(designpoint[3]) + " </lambdaQCD>"
-    pionLine = "        <pionWidthScale>" + str(designpoint[4]) + "</pionWidthScale>\n"
-    kaonLine = "        <kaonWidthScale>" + str(designpoint[5]) + "</kaonWidthScale>\n"
-    protonLine = "        <protonWidthScale>" + str(designpoint[6]) + "</protonWidthScale>\n"
-    StoUDline = "        <StoUD>" + str(designpoint[7]) + "</StoUD>\n"
-    QQtoQline = "        <QQtoQ>" + str(designpoint[8]) + "</QQtoQ>\n"
+    paramlines = []
+    for key in parameters.columns:
+        paramline = {}
+        paramline["name"] = key
+        if ":" in key:
+            paramline["line"] = "\t\t\t"+key+" = "+str(parameters.iloc[-1][key])+"\n"
+        else:
+            paramline["line"] = "\t\t\t<"+key+">"+str(parameters.iloc[-1][key])+"</"+key+">\n"
+        paramlines.append(paramline)
 
     newlines = []
     for line in xmltemplate:
-        if "<outputFilename>" in line: newlines.append(fileLine)
-        elif "<Q0>" in line: newlines.append(qLine)
-        elif "<vir_factor>" in line: newlines.append(virLine)
-        elif "<lambdaQCD>" in line: newlines.append(lambdaline)
-        elif "<pionWidthScale>" in line: newlines.append(pionLine)
-        elif "<kaonWidthScale>" in line: newlines.append(kaonLine)
-        elif "<protonWidthScale>" in line: newlines.append(protonLine)
-        elif "<StoUD>" in line: newlines.append(StoUDline)
-        elif "<QQtoQ>" in line: newlines.append(QQtoQline)
-        else: newlines.append(line)
+        if "<outputFilename>" in line:
+            newlines.append(fileLine)
+            continue
+
+        writeold = True
+        for paramline in paramlines:
+            if paramline["name"] in line: 
+                newlines.append(paramline["line"])
+                writeold = False
+                break
+
+        if writeold: newlines.append(line)
 
     xmlname = baseDir + "/config.xml"
     xml = open(xmlname,'w')
@@ -77,52 +96,24 @@ def makeDir(index):
 ######################################################################################################################################
 ##start of main method
 
-##option reading
-reading = False
-if len(sys.argv) == 2:
-    reading = True
-
 ##parameters and bounds
-points = 48
-tempdesign = lhs(8, samples = points)
-#tempdesign.sort()
-design = []
-index = 0
-for Q, vir, lQCD, pionWidth, kaonWidth, protonWidth, StoUD, QQtoQ in tempdesign:
-    ##Rescaling LHS parameters to appropriate ranges
-    Qnew = (Q*2.1 + 0.9)
-    virnew = -(vir*0.9 + 0.1)
-    lQCDnew = (lQCD*0.3 + 0.1)
-    pionWidthnew = (pionWidth*1.5 + 0.5)
-    kaonWidthnew = (kaonWidth*1.5 + 0.5)
-    protonWidthnew = (protonWidth*1.5 + 0.5)
-    StoUDnew = (StoUD*0.3+0.2)
-    QQtoQnew = (QQtoQ*0.13+0.07)
-    design.append([index, Qnew, virnew, lQCDnew, pionWidthnew, kaonWidthnew, protonWidthnew, StoUDnew, QQtoQnew])
-    index = index+1
-
-if reading:
-    design = readDesign(sys.argv[1])
+nsamples = 500
+if not reading: design = createPandaDesign(nsamples)
 
 #reading xml template
-xmltemplate = open("/scratch/user/cameron.parker/JETSCAPE-COMP-HH_colorrecomb/config/jetscape_user.xml","r")
+xmltemplate = open("/scratch/user/cameron.parker/newJETSCAPE/JETSCAPE/config/jetscape_user.xml","r")
 xmllines = xmltemplate.readlines()
 
 ##Changing to build directory
-os.chdir("/scratch/user/cameron.parker/JETSCAPE-COMP-HH_colorrecomb/build")
+os.chdir("/scratch/user/cameron.parker/newJETSCAPE/JETSCAPE/build")
 
-##setting parameters to run analysis over
-newqvirlines = [ '# Version 1.0\n', '# Parameter q0 virFac lambdaQCD pionScale kaonScale protonScale StoUD QQtoQ\n']
-for index, Q, vir, lqcd, pionWidth, kaonWidth, protonWidth, StoUD, QQtoQ in design:
-    newqvirlines.append(str(Q)+" "+str(vir)+" "+str(lqcd)+" "+str(pionWidth)+" "+str(kaonWidth)+" "+str(protonWidth)+" "+str(StoUD)+" "+str(QQtoQ)+"\n")
-
-qvirCode = open(totaldir + '/QVir_Analysis/parameters.txt','w')
-qvirCode.writelines(newqvirlines)
-qvirCode.close()
+# Loop over design points and make parameter file
+design.to_csv(totaldir+'QVir_Analysis/parameters.txt')
+print(design)
 
 ##Running jetscape for each set of parameters with multiprocessing
-pool = mp.Pool(points)
-pool.starmap(binrun, [(point, xmllines) for point in design])
+pool = mp.Pool(48)
+pool.starmap(binrun, [(i,design.loc[[i]], xmllines) for i in range(len(design))])
 pool.close()
 
 ##running total analysis
