9d78a67667196ccd23e548089dc4b913d85bbe3e
Cameron Parker <cameron.parker@tamu.edu>
2023-10-18 13:40:24 -0500

    adding partons and abs factors to functions

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
+from math import sqrt
 import os
 import sys
 from pyDOE import lhs 
 import numpy as np
 import random
 import pandas as pd
+import math
 
 # make base directories for the analysis
 def makeTotalDir(totaldir):
@@ -14,11 +16,6 @@ def makeTotalDir(totaldir):
 
     try:
         os.makedirs(totaldir + "points/")
-    except:
-        pass
-
-    try:
-        os.makedirs(totaldir + "QVir_Analysis/")
     except:
         pass
 
@@ -87,3 +84,64 @@ def concatDats(dir):
         os.remove(dat)
 
     os.chdir(startdir)
+
+class parton:
+    def __init__(self, index, PID, status, E, Px, Py, Pz):
+        self.index = index
+        self.PID = PID
+        self.status = status
+        self.E = E
+        self.Px = Px
+        self.Py = Py
+        self.Pz = Pz
+        self.pT = sqrt(Px**2 + Py**2)
+
+    def __init__(self, line):
+        params = line.split(' ')
+        self.index = int(params[0])
+        self.PID = int(params[1])
+        self.status = int(params[2])
+        self.E = float(params[3])
+        self.Px = float(params[4])
+        self.Py = float(params[5])
+        self.Pz = float(params[6])
+        self.pT = sqrt(self.Px**2 + self.Py**2)
+
+    def __str__(self):
+        return f"{self.PID} {self.E} {self.pT}"
+    
+##chance for photon to be absorbed from https:##arxiv.org/abs/hep-ph/9405309
+def absFactor1(pVec, T):
+    alpha = 1./137.
+    alphaS = 0.3
+    p = pVec.P()
+    return 2.0*(5.*math.pi/9.)*(alpha*alphaS*T*T/p)*math.log(0.2317*p/(alphaS*T))
+
+def absFactor2(pVec, T):
+    ## Constants
+    alpha = 1.0 / 137.0 
+    hbc = 0.1973 
+    
+    ## Calculate alpha_s at temperature 'temp'
+    alphsT = 6.0 * math.pi / (27.0 * math.log(T / 0.022)) 
+    gsT = math.sqrt(alphsT * 4.0 * math.pi) 
+    
+    ## Calculate prfph
+    prfph = alpha * alphsT / (math.pi * math.pi) * T * T * (6.0 / 9.0) 
+    
+    ## Calculate x and exponential term
+    p = pVec.P() 
+    x = pVec.P() / T 
+    expo = math.exp(-x) 
+    fermi = expo / (1.0 + expo) 
+    
+    ## Calculate C22 and Cab
+    C22 = 0.041 / x - 0.3615 + 1.01 * math.exp(-1.35 * x) 
+    Cab = math.sqrt(1.5) * (0.548 / pow(x, 1.5) * math.log(12.28 + 1.0 / x) + 0.133 * x / math.sqrt(1.0 + x / 16.27)) 
+    
+    ## Calculate Ctot
+    Ctot = 0.5 * math.log(2.0 * x) + C22 + Cab 
+    
+    ## Calculate dRd3p
+    dRd3p = prfph * fermi * (math.log(math.sqrt(3.0) / gsT) + Ctot) / p
+    return dRd3p * 4 * pow(math.pi,3) / expo
