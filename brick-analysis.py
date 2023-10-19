a196cd9d33cfaadcf905ebed7520b2315ba64d0f
Cameron Parker <cameron.parker@tamu.edu>
2023-10-19 12:35:34 -0500

    adding pT spread functionality

--- brick-analysis.py
+++ brick-analysis.py
@@ -13,9 +13,15 @@ from functions import parton
 analysisDir = sys.argv[1]
 directories = getDirs(analysisDir)
 outdir = analysisDir+"analysis/"
+outfile = ROOT.TFile.Open(outdir+'analysis.root', "RECREATE")
 
 # initializing histograms
-hist = ROOT.TH1D('Photon Rate','Photon Rate; Brick Length (fm); Photons Out',6,1.0,13.0)
+#pTs = [1,3,5,7,9]
+pTs = [0.5,1.5]
+lengths = [1,3,5,7,9,11,13]
+totalhist = ROOT.TH2D('Photon Rate','Photon Rate; Brick Length (fm); pT (GeV); Photons Out (%)',
+                      len(lengths)-1,lengths[0],lengths[len(lengths)-1],
+                      len(pTs)-1,pTs[0],pTs[len(pTs)-1])
 eventcounts = []
 
 # directory loop
@@ -41,25 +47,31 @@ for dir in directories:
 
         part = parton(line)
         if part.PID == 22 and part.E > 0.95:
-            hist.Fill(length)
+            totalhist.Fill(length,part.pT)
 
     eventcounts.append(events)
 
-# fits
-hist.Scale(100.0/eventcounts[0])
-hist.Fit("expo")
-hist.SetLineWidth(3)
-rp = ROOT.TRatioPlot(hist)
-fit = hist.GetFunction("expo")
 
-# output
-print("Mean free path: " + str(-1.0/fit.GetParameter(1)) + " +/- " + str(-1*fit.GetParError(1)/fit.GetParameter(1)) + " fm")
-c1 = ROOT.TCanvas("c1","c1",1400,1200)
-rp.Draw()
-c1.Print(outdir+"fit.png")
-c1.Close()
+# fits
+totalhist.Scale(100.0/eventcounts[0])
+for i in range(len(pTs)-1):
+    name = str(pTs[i])+"to"+str(pTs[i+1])
+    hist = totalhist.ProjectionX(name,i+1,i+1)
+
+    hist.GetYaxis().SetTitle("Photons Out (%)") 
+    hist.Fit("expo")
+    hist.SetLineWidth(3)
+    rp = ROOT.TRatioPlot(hist)
+    fit = hist.GetFunction("expo")
+
+    # output
+    print("Mean free path: " + str(-1.0/fit.GetParameter(1)) + " +/- " + str(-1*fit.GetParError(1)/fit.GetParameter(1)) + " fm")
+    c1 = ROOT.TCanvas("c1","c1",1400,1200)
+    rp.Draw()
+    c1.Print(outdir+name+".png")
+    c1.Close()
+    hist.Write()
 
 # end behavior
-outfile = ROOT.TFile.Open(outdir+'analysis.root', "RECREATE")
-hist.Write()
+totalhist.Write()
 outfile.Close()
