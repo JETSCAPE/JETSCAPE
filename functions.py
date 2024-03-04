64cab2571c2234ca38513046bd26febbc4b01d74
Cameron Parker <cameron.parker@tamu.edu>
2024-03-04 09:27:43 -0600

    updates to total design

--- functions.py
+++ functions.py
@@ -98,14 +98,14 @@ def createDesign(points, designparams):
 def createPandaDesign(points):
     # design parameters
     designparams = [
-        {"name": "Q0", "range": (1.2,3.0)},
+        {"name": "MultipartonInteractions:ecmPow", "range": (0.1,0.5)},
+        {"name": "MultipartonInteractions:pT0Ref", "range": (1.0,5.0)},
+        {"name": "Q0", "range": (0.95,3.0)},
         {"name": "vir_factor", "range": (0.1,1.0)},
         {"name": "ee_vir_factor", "range": (0.1,1.0)},
         {"name": "lambdaQCD", "range": (0.1,0.45)},
         {"name": "StringFlav:probStoUD", "range": (0.2,0.5)},
         {"name": "StringFlav:probQQtoQ", "range": (0.07,0.2)},
-        {"name": "MultipartonInteractions:ecmPow", "range": (0.1,0.5)},
-        {"name": "MultipartonInteractions:pT0Ref", "range": (1.0,5.0)},
     ]
 
     # initialization
