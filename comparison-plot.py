86c0759c87cc2fe2c6e53d8016ca7b9734ae6142
Cameron Parker <cameron.parker@tamu.edu>
2024-03-19 10:34:42 -0500

    switching to file input reading

--- comparison-plot.py
+++ comparison-plot.py
@@ -1,33 +1,43 @@
 import ROOT
 from functions import getRootFiles
 
+#input reading
+input = open("test/comparison-input.txt","r")
+inputLines = input.readlines()
+inputLines = [line.rstrip() for line in inputLines]
+
 # file calling
-normalDfile = ROOT.TFile("test/Dtotals.root")
-norecoDfile = ROOT.TFile("runs/Dnoreco/points/0/root/totals.root")
+normalDfile = ROOT.TFile(inputLines[1])
+norecoDfile = ROOT.TFile(inputLines[3])
 histlist = getRootFiles(normalDfile)
 
 # looping over individual plots
 for hist in histlist:
     # skipping plots for other stuff
-    if "GeV" not in hist:
+    if " vs " in hist:
         continue
 
     normalHist = normalDfile.Get(hist)
-    normalHist.SetName("Normal")
+    normalHist.SetName(inputLines[0])
     normalHist.SetLineColor(ROOT.kRed)
 
     norecoHist = norecoDfile.Get(hist)
-    norecoHist.SetName("No reco")
+    norecoHist.SetName(inputLines[2])
 
-    stack = ROOT.THStack(hist,hist+";\Delta\phi;1/N_{D}dN_{pairs}/d\Delta\phi")
-    stack.Add(normalHist)
+    title = hist+";"+normalHist.GetXaxis().GetTitle()+";"+normalHist.GetYaxis().GetTitle()
+    stack = ROOT.THStack(hist,title)
     stack.Add(norecoHist)
+    stack.Add(normalHist)
 
     legend = ROOT.TLegend(0.8 ,0.8, 0.9, 0.9)
-    legend.AddEntry(normalHist, "Total" )
-    legend.AddEntry(norecoHist, "No Reco" )
+    legend.AddEntry(normalHist, inputLines[0])
+    legend.AddEntry(norecoHist, inputLines[2])
 
     c1 = ROOT.TCanvas()
     stack.Draw("nostack")
     legend.Draw("same")
-    c1.Print("test/Drecocomparison/"+hist+".png")
+
+    if "Spectrum" in hist:
+        c1.SetLogy()
+        c1.SetLogx()
+    c1.Print(inputLines[4]+hist+".png")
