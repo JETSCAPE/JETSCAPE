3f6485beeaa403598d3e285e5d3f9c1b19ed4542
Cameron Parker <cameron.parker@tamu.edu>
2024-03-27 10:34:18 -0500

    adding file reading for params

--- comparison-plot.py
+++ comparison-plot.py
@@ -16,28 +16,33 @@ for hist in histlist:
     # skipping plots for other stuff
     if " vs " in hist:
         continue
+    if "Spectrum" not in hist:
+        continue
 
     normalHist = normalDfile.Get(hist)
     normalHist.SetName(inputLines[0])
     normalHist.SetLineColor(ROOT.kRed)
+    normalHist.SetStats(0)
 
     norecoHist = norecoDfile.Get(hist)
     norecoHist.SetName(inputLines[2])
+    norecoHist.SetStats(0)
 
     title = hist+";"+normalHist.GetXaxis().GetTitle()+";"+normalHist.GetYaxis().GetTitle()
-    stack = ROOT.THStack(hist,title)
-    stack.Add(norecoHist)
-    stack.Add(normalHist)
+    #stack = ROOT.THStack(hist,title)
+    #stack.Add(norecoHist)
+    #stack.Add(normalHist)
 
-    legend = ROOT.TLegend(0.8 ,0.8, 0.9, 0.9)
+    legend = ROOT.TLegend(0.8,0.8,0.9,0.9)
     legend.AddEntry(normalHist, inputLines[0])
     legend.AddEntry(norecoHist, inputLines[2])
 
     c1 = ROOT.TCanvas()
-    stack.Draw("nostack")
-    legend.Draw("same")
-
     if "Spectrum" in hist:
         c1.SetLogy()
         c1.SetLogx()
+    #stack.Draw("nostack")
+    rp = ROOT.TRatioPlot(norecoHist,normalHist)
+    rp.Draw()
+    legend.Draw("same")
     c1.Print(inputLines[4]+hist+".png")
