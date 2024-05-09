c5f4a02586bfff81862bf64d8ddfe7d37be6a449
Cameron Parker <cameron.parker@tamu.edu>
2024-05-09 11:00:21 -0500

    upping disk request and memory request

--- make-pp-bins.py
+++ make-pp-bins.py
@@ -89,8 +89,8 @@ testjob = htcondor.Submit({
     "error": "/data/rjfgroup/rjf01/cameron.parker/condor/cat-$(ProcId).err",
     "log": "/data/rjfgroup/rjf01/cameron.parker/condor/cat.log",
     "request_cpus": "1",
-    "request_memory": "128MB",
-    "request_disk": "128MB",
+    "request_memory": "200MB",
+    "request_disk": "500MB",
 })
 schedd = htcondor.Schedd()                   # get the Python representation of the scheduler
 
