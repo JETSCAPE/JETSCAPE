83779697d12ea26f9e1a979eef1ead1a9f5c6309
Cameron Parker <cameron.parker@tamu.edu>
2024-05-14 15:21:49 -0500

    limiting lambdaQCD range

--- functions.py
+++ functions.py
@@ -104,7 +104,7 @@ def createPandaDesign(points):
         {"name": "QSfactor", "range": (0,1)},
         {"name": "vir_factor", "range": (0.1,1.0)},
         {"name": "ee_vir_factor", "range": (0.1,1.0)},
-        {"name": "lambdaQCD", "range": (0.1,0.45)},
+        {"name": "lambdaQCD", "range": (0.1,0.4)},
         {"name": "StringFlav:probStoUD", "range": (0.0,0.5)},
         {"name": "StringFlav:probQQtoQ", "range": (0.0,0.4)},
     ]
