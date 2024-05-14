a58da8cf2580f0ce7ea864b0662f88413a572562
Cameron Parker <cameron.parker@tamu.edu>
2024-05-14 18:49:04 -0500

    reworking xml runner

--- pp-xml-run.py
+++ pp-xml-run.py
@@ -1,11 +1,26 @@
-#running over points for pp collisions
+# running over points for pp collisions
+# call with python3 pp-xml-run.py [DIRECTORY] [START] [END]
 
 import os
-import multiprocessing as mp
 from functions import *
+import htcondor
 
 totdir = sys.argv[1]
 dirs = getDirs(totdir)
+
+# job initialization
+testjob = htcondor.Submit({
+    "executable": "/data/rjfgroup/rjf01/cameron.parker/builds/JETSCAPE/build/runJetscape",
+    "arguments": "$(xml)",          # we will pass in the value for this macro via itemdata
+    "output": "/data/rjfgroup/rjf01/cameron.parker/condor/cat-$(ProcId).out",
+    "error": "/data/rjfgroup/rjf01/cameron.parker/condor/cat-$(ProcId).err",
+    "log": "/data/rjfgroup/rjf01/cameron.parker/condor/cat.log",
+    "request_cpus": "1",
+    "request_memory": "500MB",
+    "request_disk": "500MB",
+    "max_retries": "2",
+})
+schedd = htcondor.Schedd()                   # get the Python representation of the scheduler
 
 # selecting range to run over
 start = 0
@@ -18,22 +33,15 @@ if len(sys.argv) == 4:
 
 print("From " + str(start) + " to " + str(finish) + " in " + totdir + ":")
 
-# Changing to build directory
-os.chdir("/scratch/user/cameron.parker/projects/JETSCAPE/build")
-pool = mp.Pool(48)
-
 # looping over points
 for dir in dirs[start:finish]:
-    print("Running " + dir + ":")
+    print("Jobs sumbitted for:", dir)
     pointdir = totdir+"points/"+dir
-    unzipxmls(pointdir)
     xmls = readXmls(pointdir)
-    pool.map(runxml, xmls)
-    zipxmls(pointdir)
+    xmlinput = [{"xml": xml} for xml in xmls]
+    testjob["batch_name"] = totdir + "-" + dir
+    submit_result = schedd.submit(testjob, itemdata = iter(xmlinput))
 
     # concatonating all soft bins together
-    if "_0_" in xmls[0] and "_0_" in xmls[1]:
-        dats = softCombine(pointdir)
-
-pool.close()
-    
+    #if "_0_" in xmls[0] and "_0_" in xmls[1]:
+    #    dats = softCombine(pointdir)    
