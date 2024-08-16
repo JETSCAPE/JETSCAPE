657cfda84962f7130f7e9b9c7c19cf616ca5d882
Cameron Parker <cameron.parker@tamu.edu>
2024-08-16 10:08:27 -0500

    13 TeV updates

--- execs/pp-13000-comparison.cc
+++ execs/pp-13000-comparison.cc
@@ -52,49 +52,13 @@ int main(int argc, char* argv[]){
     // Create the ROOT application environment.
     TApplication theApp("hist", &argc, argv);
 
-    //declaring vectors to hold results
-    vector<vector<double>> pionPredicts, kaonPredicts, protonPredicts, jet1Predicts, jet2Predicts, jet3Predicts;
- 
-    //grabbing graphs from each jetscape file
-    for(int i = 0; i < directories.size(); i++){
-        //reading graph
-        string filename = directories[i] + "/root/totals.root";
-        cout << filename << ": ";
-        TFile file(filename.c_str());
-        TH1D* tempHistPions = (TH1D*)file.Get("identified pions"); cout << "Got pions. ";
-        TH1D* tempHistKaons = (TH1D*)file.Get("identified kaons"); cout << "Got kaons. ";
-        TH1D* tempHistProtons = (TH1D*)file.Get("identified protons"); cout << "Got protons. ";
-        TH1D* tempHistJets1 = (TH1D*)file.Get("smooth low y jets"); cout << "Got low y jets. ";
-        TH1D* tempHistJets2 = (TH1D*)file.Get("smooth mid y jets"); cout << "Got mid y jets. ";
-        TH1D* tempHistJets3 = (TH1D*)file.Get("smooth high y jets"); cout << "Got high y jets. " << endl;
-
-        //dat file data for Bayes analysis
-        vector<double> piontemp, kaontemp, protontemp, jettemp1, jettemp2, jettemp3;
-        for(int j = 1; j <= tempHistPions->GetNbinsX(); j++) piontemp.push_back(tempHistPions->GetBinContent(j));
-        for(int j = 1; j <= tempHistKaons->GetNbinsX(); j++) kaontemp.push_back(tempHistKaons->GetBinContent(j));
-        for(int j = 1; j <= tempHistProtons->GetNbinsX(); j++) protontemp.push_back(tempHistProtons->GetBinContent(j));
-        for(int j = 1; j <= tempHistJets1->GetNbinsX(); j++) jettemp1.push_back(tempHistJets1->GetBinContent(j));
-        for(int j = 1; j <= tempHistJets2->GetNbinsX(); j++) jettemp2.push_back(tempHistJets2->GetBinContent(j));
-        for(int j = 1; j <= tempHistJets3->GetNbinsX(); j++) jettemp3.push_back(tempHistJets3->GetBinContent(j));
-        pionPredicts.push_back(piontemp);
-        kaonPredicts.push_back(kaontemp);
-        protonPredicts.push_back(protontemp);
-        jet1Predicts.push_back(jettemp1);
-        jet2Predicts.push_back(jettemp2);
-        jet3Predicts.push_back(jettemp3);
-        
-        file.Close();
-    }
-
     //making said dat files
-    chdir(input.c_str());
-    char tmp[256]; getcwd(tmp, 256); cout << "Current working directory: " << tmp << endl;
-    makeDatFile(pionPredicts, "PionSpectraPrediction", "# Version 1.0\n# Pion Spectra for parameters.txt");
-    makeDatFile(kaonPredicts, "KaonSpectraPrediction", "# Version 1.0\n# Kaon Spectra for parameters.txt");
-    makeDatFile(protonPredicts, "ProtonSpectraPrediction", "# Version 1.0\n# Proton Spectra for parameters.txt");
-    makeDatFile(jet1Predicts, "LowYJetPrediction", "# Version 1.0\n# Low Y Jet Spectra for parameters.txt");
-    makeDatFile(jet2Predicts, "MidYJetPrediction", "# Version 1.0\n# Mid Y Jet Spectra for parameters.txt");
-    makeDatFile(jet3Predicts, "HighYJetPrediction", "# Version 1.0\n# High Y Jet Spectra for parameters.txt");
+    makeObsPred(directories, input, "PionSpectraPrediction", "identified pions");
+    makeObsPred(directories, input, "KaonSpectraPrediction", "identified kaons");
+    makeObsPred(directories, input, "ProtonSpectraPrediction", "identified protons");
+    makeObsPred(directories, input, "LowYJetPrediction", "smooth low y jets");
+    makeObsPred(directories, input, "MidYJetPrediction", "smooth mid y jets");
+    makeObsPred(directories, input, "HighYJetPrediction", "smooth high y jets");
 
     //exit behavior
     return 0;
