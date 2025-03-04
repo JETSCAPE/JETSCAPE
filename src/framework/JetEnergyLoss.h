d1d7760c1c7e7c552adf3cff2d4703c2f6f74bca
ivandatta99@gmail.com <ivandatta99@gmail.com>
2025-03-03 22:23:44 -0500

    Updated Documentation
    - JetEnergyLoss.h Doxygen documentation updated
    - JetEnergyLossManager Doxygen documentation updated

--- src/framework/JetEnergyLoss.h
+++ src/framework/JetEnergyLoss.h
@@ -14,255 +14,291 @@
  * See COPYING for details.
  ******************************************************************************/
 
-#ifndef JETENERGYLOSS_H
-#define JETENERGYLOSS_H
-
-#include <random>
-#include <vector>
-
-#include "FluidCellInfo.h"
-#include "FluidDynamics.h"
-#include "JetClass.h"
-#include "JetScapeModuleBase.h"
-#include "JetScapeWriter.h"
-#include "LiquefierBase.h"
-#include "MakeUniqueHelper.h"
-#include "PartonPrinter.h"
-#include "PartonShower.h"
-
-namespace Jetscape {
-
-class JetEnergyLoss : public JetScapeModuleBase,
-                      public std::enable_shared_from_this<JetEnergyLoss> {
- public:
-  /** Default constructor. It sets the value of qhat, deltaT and maxT to -99.99,
-   * 0.0 and 0.0, respectively. Standard signal slot flags are set to false.
-   */
-  JetEnergyLoss();
-
-  /** A copy constructor for Jet Energy Loss Physics Task.
-      @param j A pointer of type JetEnergyLoss class.
-   */
-  JetEnergyLoss(const JetEnergyLoss &j);
-
-  /** Destructor
-   */
-  virtual ~JetEnergyLoss();
-
-  /** Deep copy.
-      @return Null pointer.
-   */
-  virtual shared_ptr<JetEnergyLoss> Clone() const { return nullptr; }
-
-  /** It reads the input parameters from a XML file under the tag <Eloss>.
-      Sets the Parton class @a inP and PartonShower class @a pShower to null.
-      Also initializes the tasks attached to the JetEnergyLoss module.
-  */
-  virtual void Init();
-
-  /** It calls DoShower() for all shower-initiating partons.
-      To avoid abuse, this can NOT be overwritten. Eloss happens on a
-     parton-by-parton level, Exec() should only be executed once per event.
-   */
-  virtual void Exec()
-      final;  // prevents eloss modules from overwrting and missusing
-
-  /** Write output information for each tasks/subtasks attached to the
-     JetEnergyLoss module using JetScapeWriter functionality.
-      @param w A pointer of type JetScapeWriter.
-  */
-  virtual void WriteTask(weak_ptr<JetScapeWriter> w);
-
-  /** Reset the parton shower information.
-   */
-  virtual void Clear();
-
-  /** Default function to perform the energy loss for partons at time "time". It
-     should be overridden by different energy loss tasks.
-      @param deltaT Step-size.
-      @param time Current time.
-      @param Q2 Current virtuality of the parton.
-      @param pIn Vector of current partons.
-      @param pOut Vector of partons at time "time+deltaT".
-   */
-  virtual void DoEnergyLoss(double deltaT, double time, double Q2,
-                            vector<Parton> &pIn, vector<Parton> &pOut){};
-
-  //! Core signal to receive information from the medium
-  sigslot::signal5<double, double, double, double,
-                   std::unique_ptr<FluidCellInfo> &, multi_threaded_local>
-      GetHydroCellSignal;
-
-  sigslot::signal1<double &, multi_threaded_local> GetHydroTau0Signal;
-
-  /** For future development. A signal to connect the JetEnergyLoss object to
-   * the function UpdateEnergyDeposit() of the FluidDynamics class.
-   */
-  sigslot::signal2<int, double, multi_threaded_local> jetSignal;
-
-  /** For future development. A signal to connect the JetEnergyLoss object to
-   * the function GetEnergyDensity() of the FluidDynamics class.
-   */
-  sigslot::signal2<int, double &, multi_threaded_local> edensitySignal;
-
-  /** A signal to connect the JetEnergyLoss object to the function
-     DoEnergyLoss() function. Send all a list of shower-initiating partons to
-     all attached eloss modules. They in turn decide whether they are
-     responsible or not.
-      @TODO Rename...
-   */
-  sigslot::signal5<double, double, double, vector<Parton> &, vector<Parton> &,
-                   multi_threaded_local>
-      SentInPartons;
-
-  /** Sets the value of qhat to "m_qhat".
-      @param m_qhat Jet quenching parameter q-hat.
-   */
-  void SetQhat(double m_qhat) { qhat = m_qhat; }
-
-  /** @return The current value of qhat.
-   */
-  const double GetQhat() const { return qhat; }
-
-  /** It adds a initiating parton @a p to create the parton shower in an energy
-     loss task.
-      @param p A pointer of type parton class.
-   */
-  void AddShowerInitiatingParton(shared_ptr<Parton> p) { inP = p; }
-
-  /** @return The parton which initiated the parton shower.
-   */
-  shared_ptr<Parton> GetShowerInitiatingParton() { return inP; }
-
-  void PrintShowerInitiatingParton();
-
-  /** @return The time-step "deltaT" used by energy loss task.
-   */
-  double GetDeltaT() { return deltaT; }
-
-  /** @return The maximum time limit for parton shower.
-   */
-  double GetMaxT() { return maxT; }
-
-  /** @return The current shower.
-   */
-  shared_ptr<PartonShower> GetShower() { return pShower; }
-
-  // old test signals ======================
-  //! TODO: Remove
-  /** Set the flag m_jetSignalConnected to true, if JetEnergyLoss had sent a
-     signal to the function UpdateEnergyDeposit() of the class FluidDynamics.
-      @param m_jetSignalConnected A boolean flag.
-   */
-  void SetJetSignalConnected(bool m_jetSignalConnected) {
-    jetSignalConnected = m_jetSignalConnected;
-  }
-
-  /**  @return A boolean flag. Its status indicates whether JetEnergyLoss had
-   * sent a signal to the function UpdateEnergyDeposit() of the class
-   * FluidDynamics.
-   */
-  const bool GetJetSignalConnected() const { return jetSignalConnected; }
-
-  /** Set the flag m_edensitySignalConnected to true, if JetEnergyLoss had sent
-     a signal to the function GetEnergyDensity() of the class FluidDynamics.
-      @param m_edensitySignalConnected A boolean flag.
-   */
-  void SetEdensitySignalConnected(bool m_edensitySignalConnected) {
-    edensitySignalConnected = m_edensitySignalConnected;
-  }
-
-  /**
-     @return A boolean flag. Its status indicates whether JetEnergyLoss had sent
-     a signal to the function GetEnergyDensity() of the class FluidDynamics.
-   */
-  const bool GetEdensitySignalConnected() const {
-    return edensitySignalConnected;
-  }
-
-  /** Set the flag m_GetHydroCellSignalConnected to true, if JetEnergyLoss had
-     sent a signal to the function GetHydroCell() of the class FluidDynamics.
-      @param m_GetHydroCellSignalConnected A boolean flag.
-   */
-  void SetGetHydroCellSignalConnected(bool m_GetHydroCellSignalConnected) {
-    GetHydroCellSignalConnected = m_GetHydroCellSignalConnected;
-  }
-
-  /**
-     @return A boolean flag. Its status indicates whether JetEnergyLoss had sent
-     a signal to the function GetHydroCell() of the class FluidDynamics.
-   */
-  const bool GetGetHydroCellSignalConnected() {
-    return GetHydroCellSignalConnected;
-  }
-
-  void SetGetHydroTau0SignalConnected(bool m_GetHydroTau0SignalConnected) {
-    GetHydroTau0SignalConnected = m_GetHydroTau0SignalConnected;
-  }
-
-  const bool GetGetHydroTau0SignalConnected() {
-    return GetHydroTau0SignalConnected;
-  }
-
-  /** Set the flag m_SentInPartonsConnected to true, if JetEnergyLoss had sent a
-     signal to the function DoEnergyLoss().
-      @param m_SentInPartonsConnected A boolean flag.
-   */
-  void SetSentInPartonsConnected(bool m_SentInPartonsConnected) {
-    SentInPartonsConnected = m_SentInPartonsConnected;
-  }
-
-  /**
-      @return A boolean flag. Its status indicates whether JetEnergyLoss had
-     sent a signal to the function DoEnergyLoss().
-   */
-  const bool GetSentInPartonsConnected() { return SentInPartonsConnected; }
-
-  void add_a_liquefier(std::shared_ptr<LiquefierBase> new_liquefier) {
-    liquefier_ptr = new_liquefier;
-  }
-
-  std::weak_ptr<LiquefierBase> get_liquefier() { return (liquefier_ptr); }
-
-  // The Slot method to send the vector of Hadronization module
-  void SendFinalStatePartons(vector<vector<shared_ptr<Parton>>> &fPartons) {
-    fPartons = final_Partons;
-  }
-
-  void GetFinalPartonsForEachShower(shared_ptr<PartonShower> shower);
-
- protected:
-  std::weak_ptr<LiquefierBase> liquefier_ptr;
-
- private:
-  double deltaT;
-  double maxT;
-
-  double qhat;
-  shared_ptr<Parton> inP;
-  shared_ptr<PartonShower> pShower;
-
-  bool GetHydroCellSignalConnected;
-  bool GetHydroTau0SignalConnected;
-  bool SentInPartonsConnected;
-
-  /** This function executes the shower process for the partons produced from
-   * the hard scaterring.
-   */
-  void DoShower();
-
-  node vStart;
-  node vEnd;
-
-  // old test signals
-  bool jetSignalConnected;
-  bool edensitySignalConnected;
-
-  // Vector of final state partons for each shower as a vector
-
-  vector<vector<shared_ptr<Parton>>> final_Partons;
-};
-
-}  // end namespace Jetscape
-
-#endif
+ #ifndef JETENERGYLOSS_H
+ #define JETENERGYLOSS_H
+ 
+ #include <random>
+ #include <vector>
+ 
+ #include "FluidCellInfo.h"
+ #include "FluidDynamics.h"
+ #include "JetClass.h"
+ #include "JetScapeModuleBase.h"
+ #include "JetScapeWriter.h"
+ #include "LiquefierBase.h"
+ #include "MakeUniqueHelper.h"
+ #include "PartonPrinter.h"
+ #include "PartonShower.h"
+ 
+ namespace Jetscape {
+ 
+ class JetEnergyLoss : public JetScapeModuleBase,
+                       public std::enable_shared_from_this<JetEnergyLoss> {
+  public:
+   /** 
+    * Default constructor. It sets the value of qhat, deltaT and maxT to -99.99,
+    * 0.0 and 0.0, respectively. Standard signal slot flags are set to false.
+    */
+   JetEnergyLoss();
+ 
+   /** 
+    * A copy constructor for Jet Energy Loss Physics Task.
+    * @param j A pointer of type JetEnergyLoss class.
+    */
+   JetEnergyLoss(const JetEnergyLoss &j);
+ 
+   /** 
+    * Destructor
+    */
+   virtual ~JetEnergyLoss();
+ 
+   /** 
+    * Deep copy.
+    * @return Null pointer.
+    */
+   virtual shared_ptr<JetEnergyLoss> Clone() const { return nullptr; }
+ 
+   /** 
+    * It reads the input parameters from a XML file under the tag <Eloss>.
+    * Sets the Parton class @a inP and PartonShower class @a pShower to null.
+    * Also initializes the tasks attached to the JetEnergyLoss module.
+    */
+   virtual void Init();
+ 
+   /** 
+    * It calls DoShower() for all shower-initiating partons.
+    * To avoid abuse, this can NOT be overwritten. Eloss happens on a
+    * parton-by-parton level, Exec() should only be executed once per event.
+    */
+   virtual void Exec() final;
+ 
+   /** 
+    * Write output information for each tasks/subtasks attached to the
+    * JetEnergyLoss module using JetScapeWriter functionality.
+    * @param w A pointer of type JetScapeWriter.
+    */
+   virtual void WriteTask(weak_ptr<JetScapeWriter> w);
+ 
+   /** 
+    * Reset the parton shower information.
+    */
+   virtual void Clear();
+ 
+   /** 
+    * Default function to perform the energy loss for partons at time "time". It
+    * should be overridden by different energy loss tasks.
+    * @param deltaT Step-size.
+    * @param time Current time.
+    * @param Q2 Current virtuality of the parton.
+    * @param pIn Vector of current partons.
+    * @param pOut Vector of partons at time "time+deltaT".
+    */
+   virtual void DoEnergyLoss(double deltaT, double time, double Q2,
+                             vector<Parton> &pIn, vector<Parton> &pOut){};
+ 
+   //! Core signal to receive information from the medium
+   sigslot::signal5<double, double, double, double,
+                    std::unique_ptr<FluidCellInfo> &, multi_threaded_local>
+       GetHydroCellSignal;
+ 
+   sigslot::signal1<double &, multi_threaded_local> GetHydroTau0Signal;
+ 
+   /** 
+    * For future development. A signal to connect the JetEnergyLoss object to
+    * the function UpdateEnergyDeposit() of the FluidDynamics class.
+    */
+   sigslot::signal2<int, double, multi_threaded_local> jetSignal;
+ 
+   /** 
+    * For future development. A signal to connect the JetEnergyLoss object to
+    * the function GetEnergyDensity() of the FluidDynamics class.
+    */
+   sigslot::signal2<int, double &, multi_threaded_local> edensitySignal;
+ 
+   /** 
+    * A signal to connect the JetEnergyLoss object to the function
+    * DoEnergyLoss() function. Send all a list of shower-initiating partons to
+    * all attached eloss modules. They in turn decide whether they are
+    * responsible or not.
+    * @TODO Rename...
+    */
+   sigslot::signal5<double, double, double, vector<Parton> &, vector<Parton> &,
+                    multi_threaded_local>
+       SentInPartons;
+ 
+   /** 
+    * Sets the value of qhat to "m_qhat".
+    * @param m_qhat Jet quenching parameter q-hat.
+    */
+   void SetQhat(double m_qhat) { qhat = m_qhat; }
+ 
+   /** 
+    * @return The current value of qhat.
+    */
+   const double GetQhat() const { return qhat; }
+ 
+   /** 
+    * It adds a initiating parton @a p to create the parton shower in an energy
+    * loss task.
+    * @param p A pointer of type parton class.
+    */
+   void AddShowerInitiatingParton(shared_ptr<Parton> p) { inP = p; }
+ 
+   /** 
+    * @return The parton which initiated the parton shower.
+    */
+   shared_ptr<Parton> GetShowerInitiatingParton() { return inP; }
+ 
+   void PrintShowerInitiatingParton();
+ 
+   /** 
+    * @return The time-step "deltaT" used by energy loss task.
+    */
+   double GetDeltaT() { return deltaT; }
+ 
+   /** 
+    * @return The maximum time limit for parton shower.
+    */
+   double GetMaxT() { return maxT; }
+ 
+   /** 
+    * @return The current shower.
+    */
+   shared_ptr<PartonShower> GetShower() { return pShower; }
+ 
+   // old test signals ======================
+   //! TODO: Remove
+   /** 
+    * Set the flag m_jetSignalConnected to true, if JetEnergyLoss had sent a
+    * signal to the function UpdateEnergyDeposit() of the class FluidDynamics.
+    * @param m_jetSignalConnected A boolean flag.
+    */
+   void SetJetSignalConnected(bool m_jetSignalConnected) {
+     jetSignalConnected = m_jetSignalConnected;
+   }
+ 
+   /** 
+    * @return A boolean flag. Its status indicates whether JetEnergyLoss had
+    * sent a signal to the function UpdateEnergyDeposit() of the class
+    * FluidDynamics.
+    */
+   const bool GetJetSignalConnected() const { return jetSignalConnected; }
+ 
+   /** 
+    * Set the flag m_edensitySignalConnected to true, if JetEnergyLoss had sent
+    * a signal to the function GetEnergyDensity() of the class FluidDynamics.
+    * @param m_edensitySignalConnected A boolean flag.
+    */
+   void SetEdensitySignalConnected(bool m_edensitySignalConnected) {
+     edensitySignalConnected = m_edensitySignalConnected;
+   }
+ 
+   /** 
+    * @return A boolean flag. Its status indicates whether JetEnergyLoss had sent
+    * a signal to the function GetEnergyDensity() of the class FluidDynamics.
+    */
+   const bool GetEdensitySignalConnected() const {
+     return edensitySignalConnected;
+   }
+ 
+   /** 
+    * Set the flag m_GetHydroCellSignalConnected to true, if JetEnergyLoss had
+    * sent a signal to the function GetHydroCell() of the class FluidDynamics.
+    * @param m_GetHydroCellSignalConnected A boolean flag.
+    */
+   void SetGetHydroCellSignalConnected(bool m_GetHydroCellSignalConnected) {
+     GetHydroCellSignalConnected = m_GetHydroCellSignalConnected;
+   }
+ 
+   /** 
+    * @return A boolean flag. Its status indicates whether JetEnergyLoss had sent
+    * a signal to the function GetHydroCell() of the class FluidDynamics.
+    */
+   const bool GetGetHydroCellSignalConnected() {
+     return GetHydroCellSignalConnected;
+   }
+ 
+   void SetGetHydroTau0SignalConnected(bool m_GetHydroTau0SignalConnected) {
+     GetHydroTau0SignalConnected = m_GetHydroTau0SignalConnected;
+   }
+ 
+   const bool GetGetHydroTau0SignalConnected() {
+     return GetHydroTau0SignalConnected;
+   }
+ 
+   /** 
+    * Set the flag m_SentInPartonsConnected to true, if JetEnergyLoss had sent a
+    * signal to the function DoEnergyLoss().
+    * @param m_SentInPartonsConnected A boolean flag.
+    */
+   void SetSentInPartonsConnected(bool m_SentInPartonsConnected) {
+     SentInPartonsConnected = m_SentInPartonsConnected;
+   }
+ 
+   /** 
+    * @return A boolean flag. Its status indicates whether JetEnergyLoss had
+    * sent a signal to the function DoEnergyLoss().
+    */
+   const bool GetSentInPartonsConnected() { return SentInPartonsConnected; }
+ 
+   /** 
+    * Adds a liquefier to the JetEnergyLoss module.
+    * This function sets the liquefier pointer to the provided liquefier object.
+    * @param new_liquefier A shared pointer to a LiquefierBase object.
+    */
+   void add_a_liquefier(std::shared_ptr<LiquefierBase> new_liquefier) {
+     liquefier_ptr = new_liquefier;
+   }
+ 
+   /** 
+    * Retrieves the liquefier associated with the JetEnergyLoss module.
+    * This function returns a weak pointer to the liquefier object.
+    * @return A weak pointer to a LiquefierBase object.
+    */
+   std::weak_ptr<LiquefierBase> get_liquefier() { return (liquefier_ptr); }
+ 
+   /** 
+    * The Slot method to send the vector of partons to Hadronization module.
+    * @param fPartons A reference to a vector of vectors of shared pointers to Parton objects.
+    */
+   void SendFinalStatePartons(vector<vector<shared_ptr<Parton>>> &fPartons) {
+     fPartons = final_Partons;
+   }
+ 
+   void GetFinalPartonsForEachShower(shared_ptr<PartonShower> shower);
+ 
+  protected:
+   std::weak_ptr<LiquefierBase> liquefier_ptr;
+ 
+  private:
+   double deltaT;  // size of time step during evolution in fm/c
+   double maxT;    // maximum time limit for parton shower in fm/c
+ 
+   double qhat;
+   shared_ptr<Parton> inP;
+   shared_ptr<PartonShower> pShower;
+ 
+   bool GetHydroCellSignalConnected;
+   bool GetHydroTau0SignalConnected;
+   bool SentInPartonsConnected;
+ 
+   /** 
+    * This function executes the shower process for the partons produced from
+    * the hard scattering.
+    */
+   void DoShower();
+ 
+   node vStart;
+   node vEnd;
+ 
+   // old test signals
+   bool jetSignalConnected;
+   bool edensitySignalConnected;
+ 
+   // Vector of final state partons for each shower as a vector
+   vector<vector<shared_ptr<Parton>>> final_Partons;
+ };
+ 
+ }  // end namespace Jetscape
+ 
+ #endif
--- src/framework/JetEnergyLossManager.h
+++ src/framework/JetEnergyLossManager.h
@@ -14,97 +14,112 @@
  * See COPYING for details.
  ******************************************************************************/
 
-#ifndef JETENERGYLOSSMANAGER_H
-#define JETENERGYLOSSMANAGER_H
-
-#include <vector>
-
-#include "JetClass.h"
-#include "JetScapeTask.h"
-#include "sigslot.h"
-
-namespace Jetscape {
-/** @class Jet energy loss manager.
- */
-class JetEnergyLossManager
-    : public JetScapeTask,
-      public std::enable_shared_from_this<JetEnergyLossManager> {
- public:
-  /** Default constructor to create a jet energy loss manager. Sets task ID as
-   * "JLossManager". Flag GetHardPartonListConnected is set to false.
-   */
-  JetEnergyLossManager();
-
-  /** Destructor for the jet energy loss manager.
-   */
-  virtual ~JetEnergyLossManager();
-
-  /** It initializes the tasks attached to the jet energy loss manager. It also
-     sends a signal to connect the JetEnergyLoss object to the
-     GetHardPartonList() function of the HardProcess class. It can be overridden
-     by other tasks.
-      @sa JetScapeSignalManager to understand the implementation of signal slots
-     philosophy.
-   */
-  virtual void Init();
-
-  /** It reads the Hard Patrons list and calls CreateSignalSlots() function.
-   * Then, it executes the energy loss tasks attached with the jet energy loss
-   * manager. This function also includes the parallel computing feature. It can
-   * be overridden by other tasks.
-   */
-  virtual void Exec();
-
-  /** It erases the tasks attached with the energy loss manager. It can be
-   * overridden by other tasks.
-   */
-  virtual void Clear();
-
-  /** It writes the output information relevant to the jet energy loss
-     tasks/subtasks into a file. It can be overridden by other tasks.
-      @param w A pointer of type JetScapeWriter class.
-      @sa JetScapeWriter class for further information.
+ #ifndef JETENERGYLOSSMANAGER_H
+ #define JETENERGYLOSSMANAGER_H
+ 
+ #include <vector>
+ 
+ #include "JetClass.h"
+ #include "JetScapeTask.h"
+ #include "sigslot.h"
+ 
+ namespace Jetscape {
+ 
+ /**
+  * @class JetEnergyLossManager
+  * @brief Manages the jet energy loss tasks.
   */
-  virtual void WriteTask(weak_ptr<JetScapeWriter> w);
-
-  int GetNumSignals();
-
-  /** Uses philosophy of signal slots. Checks whether the attached task is
-     connected via signal slots to the functions UpdateEnergyDeposit(),
-     GetEnergyDensity(), GetHydroCell() (defined in FluidDynamics class), and
-     DoEnergyLoss() (defined in JetEnergyLoss class). If not, then, it sends a
-     signal to these functions.
-      @sa JetScapeSignalManager to understand the implementation of signal slots
-     philosophy.
-   */
-  void CreateSignalSlots();
-
-  /** A signal to connect the JetEnergyLossManager to the function
-   * GetHardPartonList() of the class HardProcess.
-   */
-  sigslot::signal1<vector<shared_ptr<Parton>> &> GetHardPartonList;
-
-  /** Use the flag m_GetHardPartonListConnected as true, if JetEnergyLossManager
-     had sent a signal to function GetHardPartonList() of the class HardProcess.
-      @param m_GetHardPartonListConnected A boolean flag.
-   */
-  void SetGetHardPartonListConnected(bool m_GetHardPartonListConnected) {
-    GetHardPartonListConnected = m_GetHardPartonListConnected;
-  }
-
-  /** @return GetHardPartonListConnected A boolean flag. Its status indicates
-   * whether JetEnergyLossManager had sent a signal to the function
-   * GetHardPartonList() of the class HardProcess.
-   */
-  const bool GetGetHardPartonListConnected() {
-    return GetHardPartonListConnected;
-  }
-
- private:
-  bool GetHardPartonListConnected;
-  vector<shared_ptr<Parton>> hp;
-};
-
-}  // end namespace Jetscape
-
-#endif
+ class JetEnergyLossManager
+     : public JetScapeTask,
+       public std::enable_shared_from_this<JetEnergyLossManager> {
+  public:
+   /** 
+    * @brief Default constructor to create a jet energy loss manager.
+    * Sets task ID as "JLossManager". Flag GetHardPartonListConnected is set to false.
+    */
+   JetEnergyLossManager();
+ 
+   /** 
+    * @brief Destructor for the jet energy loss manager.
+    */
+   virtual ~JetEnergyLossManager();
+ 
+   /** 
+    * @brief Initializes the tasks attached to the jet energy loss manager.
+    * It also sends a signal to connect the JetEnergyLoss object to the
+    * GetHardPartonList() function of the HardProcess class. It can be overridden
+    * by other tasks.
+    * @sa JetScapeSignalManager to understand the implementation of signal slots philosophy.
+    */
+   virtual void Init();
+ 
+   /** 
+    * @brief Reads the Hard Patrons list and calls CreateSignalSlots() function.
+    * Then, it executes the energy loss tasks attached with the jet energy loss
+    * manager. This function also includes the parallel computing feature. It can
+    * be overridden by other tasks.
+    */
+   virtual void Exec();
+ 
+   /** 
+    * @brief Erases the tasks attached with the energy loss manager.
+    * It can be overridden by other tasks.
+    */
+   virtual void Clear();
+ 
+   /** 
+    * @brief Writes the output information relevant to the jet energy loss
+    * tasks/subtasks into a file. It can be overridden by other tasks.
+    * @param w A pointer of type JetScapeWriter class.
+    * @sa JetScapeWriter class for further information.
+    */
+   virtual void WriteTask(weak_ptr<JetScapeWriter> w);
+ 
+   /**
+    * @brief Gets the number of signals.
+    * @return The number of signals.
+    */
+   int GetNumSignals();
+ 
+   /** 
+    * @brief Uses philosophy of signal slots. Checks whether the attached task is
+    * connected via signal slots to the functions UpdateEnergyDeposit(),
+    * GetEnergyDensity(), GetHydroCell() (defined in FluidDynamics class), and
+    * DoEnergyLoss() (defined in JetEnergyLoss class). If not, then, it sends a
+    * signal to these functions.
+    * @sa JetScapeSignalManager to understand the implementation of signal slots philosophy.
+    */
+   void CreateSignalSlots();
+ 
+   /** 
+    * @brief A signal to connect the JetEnergyLossManager to the function
+    * GetHardPartonList() of the class HardProcess.
+    */
+   sigslot::signal1<vector<shared_ptr<Parton>> &> GetHardPartonList;
+ 
+   /** 
+    * @brief Use the flag m_GetHardPartonListConnected as true, if JetEnergyLossManager
+    * had sent a signal to function GetHardPartonList() of the class HardProcess.
+    * @param m_GetHardPartonListConnected A boolean flag.
+    */
+   void SetGetHardPartonListConnected(bool m_GetHardPartonListConnected) {
+     GetHardPartonListConnected = m_GetHardPartonListConnected;
+   }
+ 
+   /** 
+    * @brief Gets the status of GetHardPartonListConnected flag.
+    * @return A boolean flag. Its status indicates whether JetEnergyLossManager had
+    * sent a signal to the function GetHardPartonList() of the class HardProcess.
+    */
+   const bool GetGetHardPartonListConnected() {
+     return GetHardPartonListConnected;
+   }
+ 
+  private:
+   bool GetHardPartonListConnected; ///< Flag indicating if GetHardPartonList is connected.
+   vector<shared_ptr<Parton>> hp; ///< Vector of shared pointers to Parton objects.
+ };
+ 
+ }  // end namespace Jetscape
+ 
+ #endif
