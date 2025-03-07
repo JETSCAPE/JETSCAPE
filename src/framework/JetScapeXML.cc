632fc1e534f67368e0216fd1082f76465cd4e5fc
Joe Latessa <latessa@users.noreply.github.com>
2025-03-07 16:32:33 -0500

    apply Doxygen style documentation to JetScapeXML

--- src/framework/JetScapeXML.cc
+++ src/framework/JetScapeXML.cc
@@ -24,8 +24,19 @@ using namespace std;
 
 namespace Jetscape {
 
+/**
+ * @brief Singleton instance of the JetScapeXML class.
+ */
 JetScapeXML *JetScapeXML::m_pInstance = NULL;
 
+/**
+ * @brief Singleton instance accessor for JetScapeXML.
+ *
+ * This method returns the singleton instance of the JetScapeXML class.
+ * If the instance does not exist, it creates a new one and logs the creation.
+ *
+ * @return JetScapeXML* Pointer to the singleton instance of JetScapeXML.
+ */
 JetScapeXML *JetScapeXML::Instance() {
   if (!m_pInstance) {
     JSINFO << "Created JetScapeXML Instance";
@@ -35,13 +46,34 @@ JetScapeXML *JetScapeXML::Instance() {
   return m_pInstance;
 }
 
-//________________________________________________________________
+/**
+ * @brief Deprecated function to open the XML master file.
+ *
+ * This function is deprecated and should not be used. It logs a warning message
+ * and calls the OpenXMLMainFile() function instead.
+ *
+ * @deprecated Use OpenXMLMainFile() instead.
+ */
 void JetScapeXML::OpenXMLMasterFile() {
   JSWARN << "Deprecated function OpenXMLMasterFile(): Call OpenXMLMainFile() "
             "instead!";
   OpenXMLMainFile();
 }
 
+/**
+ * @brief Opens the main XML file for JetScape.
+ *
+ * This function attempts to open the main XML file specified by the
+ * GetXMLMainFileName() function. If the file is successfully opened,
+ * it sets the xml_root_main to the first "jetscape" element in the file.
+ * If the main file cannot be opened, it attempts to open a deprecated
+ * default master XML file located at "../config/jetscape_master.xml".
+ * If neither file can be opened, the function logs a warning and exits
+ * the program with an error code.
+ *
+ * @note The function sets the xml_main_file_open flag to true if the
+ * main XML file is successfully opened.
+ */
 void JetScapeXML::OpenXMLMainFile() {
   if (!IsMainFileOpen()) {
     xml_doc_main.LoadFile((char *)GetXMLMainFileName().c_str());
@@ -84,20 +116,48 @@ void JetScapeXML::OpenXMLMainFile() {
   }
 }
 
-//________________________________________________________________
+/**
+ * @brief Deprecated function to open the XML master file with a given name.
+ *
+ * This function is deprecated and should not be used. It logs a warning message
+ * and calls the OpenXMLMainFile(string) function instead.
+ *
+ * @deprecated Use OpenXMLMainFile(string) instead.
+ */
 void JetScapeXML::OpenXMLMasterFile(string m_name) {
   JSWARN << "Deprecated function OpenXMLMasterFile(): Call OpenXMLMainFile() "
             "instead!";
   OpenXMLMainFile(m_name);
 }
 
-//________________________________________________________________
+/**
+ * @brief Opens the main XML file with the given name.
+ *
+ * This function sets the main XML file name using the provided string
+ * and then opens the XML file.
+ *
+ * @param m_name The name of the XML file to be opened.
+ */
 void JetScapeXML::OpenXMLMainFile(string m_name) {
   SetXMLMainFileName(m_name);
   OpenXMLMainFile();
 }
 
-//________________________________________________________________
+/**
+ * @brief Opens the XML user file and loads its content.
+ *
+ * This function attempts to open and load the XML user file specified by
+ * GetXMLUserFileName(). If the file is successfully opened and parsed, it
+ * sets the xml_root_user to the first "jetscape" element in the file. If the
+ * file cannot be opened or parsed, or if the root element is not found, the
+ * function logs an appropriate warning message and terminates the program.
+ *
+ * The function ensures that the XML user file is only opened once by checking
+ * the xml_user_file_open flag.
+ *
+ * @note This function will terminate the program if the XML user file cannot
+ * be opened or parsed correctly, or if the root element is not found.
+ */
 void JetScapeXML::OpenXMLUserFile() {
   if (!xml_user_file_open) {
     xml_doc_user.LoadFile((char *)GetXMLUserFileName().c_str());
@@ -122,13 +182,31 @@ void JetScapeXML::OpenXMLUserFile() {
   }
 }
 
-//________________________________________________________________
+/**
+ * @brief Opens an XML user file.
+ *
+ * This function sets the XML user file name using the provided string
+ * and then opens the XML user file.
+ *
+ * @param m_name The name of the XML user file to be opened.
+ */
 void JetScapeXML::OpenXMLUserFile(string m_name) {
   SetXMLUserFileName(m_name);
   OpenXMLUserFile();
 }
 
-//________________________________________________________________
+/**
+ * @brief Deprecated function to get the XML element from the specified path.
+ *
+ * This function is deprecated and should not be used. Use GetXMLElementMain()
+ * instead.
+ *
+ * @deprecated Use GetXMLElementMain() instead.
+ *
+ * @param path An initializer list of const char* representing the path to the
+ * XML element.
+ * @return A pointer to the tinyxml2::XMLElement at the specified path.
+ */
 tinyxml2::XMLElement *JetScapeXML::GetXMLElementMaster(
     std::initializer_list<const char *> &path) {
   JSWARN << "Deprecated function GetXMLElementMaster(). Call "
@@ -136,7 +214,19 @@ tinyxml2::XMLElement *JetScapeXML::GetXMLElementMaster(
   return GetXMLElementMain(path);
 }
 
-//________________________________________________________________
+/**
+ * @brief Retrieves an XML element from the main XML file based on the provided
+ * path.
+ *
+ * This function navigates through the XML structure starting from the root
+ * element and follows the sequence of element names provided in the path to
+ * locate the desired XML element.
+ *
+ * @param path A list of element names representing the path to the desired XML
+ * element.
+ * @return A pointer to the located tinyxml2::XMLElement, or nullptr if the
+ * element is not found.
+ */
 tinyxml2::XMLElement *JetScapeXML::GetXMLElementMain(
     std::initializer_list<const char *> &path) {
   VERBOSE(2) << "Looking for element in Main file: " << path;
@@ -174,7 +264,18 @@ tinyxml2::XMLElement *JetScapeXML::GetXMLElementMain(
   return currentElement;
 }
 
-//________________________________________________________________
+/**
+ * @brief Retrieves an XML element from the user file based on the provided
+ * path.
+ *
+ * This function navigates through the XML structure using the given path and
+ * returns the corresponding XML element if found. The path is specified as an
+ * initializer list of element names.
+ *
+ * @param path An initializer list of const char* representing the path to the
+ *             desired XML element.
+ * @return A pointer to the tinyxml2::XMLElement if found, otherwise nullptr.
+ */
 tinyxml2::XMLElement *JetScapeXML::GetXMLElementUser(
     std::initializer_list<const char *> &path) {
   VERBOSE(2) << "Looking for element in User file: " << path;
@@ -212,7 +313,22 @@ tinyxml2::XMLElement *JetScapeXML::GetXMLElementUser(
   return currentElement;
 }
 
-//________________________________________________________________
+/**
+ * @brief Retrieves an XML element from the user or main XML file based on the
+ * provided path.
+ *
+ * This function attempts to find an XML element specified by the given path. It
+ * first searches in the user XML file, and if not found, it searches in the
+ * main XML file. If the element is required and not found in either file, the
+ * function logs a warning and terminates the program.
+ *
+ * @param path An initializer list of const char* representing the path to the
+ * desired XML element.
+ * @param isRequired A boolean flag indicating whether the XML element is
+ * required. Defaults to true.
+ * @return A pointer to the found tinyxml2::XMLElement, or nullptr if the
+ * element is not found and isRequired is false.
+ */
 tinyxml2::XMLElement *JetScapeXML::GetElement(
     std::initializer_list<const char *> path, bool isRequired /* = true */) {
   // Try to get value from User XML file
@@ -235,7 +351,22 @@ tinyxml2::XMLElement *JetScapeXML::GetElement(
   }
 }
 
-//________________________________________________________________
+/**
+ * @brief Retrieves the text content of an XML element specified by a path.
+ *
+ * This function navigates through the XML structure using the provided path
+ * and returns the text content of the target element. If the element is not
+ * found and isRequired is true, an exception may be thrown by GetElement.
+ * If the element is not found and isRequired is false, an empty string is
+ * returned.
+ *
+ * @param path An initializer list of const char* representing the path to the
+ * target element.
+ * @param isRequired A boolean flag indicating whether the element is required.
+ * Defaults to true.
+ * @return A std::string containing the text content of the target XML element,
+ * or an empty string if the element is not found and isRequired is false.
+ */
 std::string JetScapeXML::GetElementText(
     std::initializer_list<const char *> path, bool isRequired /* = true */) {
   tinyxml2::XMLElement *element = GetElement(path, isRequired);
@@ -247,7 +378,20 @@ std::string JetScapeXML::GetElementText(
   }
 }
 
-//________________________________________________________________
+/**
+ * @brief Retrieves the int value of an XML element specified by the given
+ * path.
+ *
+ * This function navigates through the XML structure using the provided path and
+ * retrieves the int value of the target element.
+ *
+ * @param path An initializer list of const char pointers representing the path
+ * to the target element.
+ * @param isRequired A boolean flag indicating whether the element is required.
+ * Defaults to true.
+ * @return An int value representing the content of the target XML element, or
+ * 0. if the element is not found.
+ */
 int JetScapeXML::GetElementInt(std::initializer_list<const char *> path,
                                bool isRequired /* = true */) {
   tinyxml2::XMLElement *element = GetElement(path, isRequired);
@@ -261,7 +405,20 @@ int JetScapeXML::GetElementInt(std::initializer_list<const char *> path,
   }
 }
 
-//________________________________________________________________
+/**
+ * @brief Retrieves the double value of an XML element specified by the given
+ * path.
+ *
+ * This function navigates through the XML structure using the provided path and
+ * retrieves the double value of the target element.
+ *
+ * @param path An initializer list of const char pointers representing the path
+ * to the target element.
+ * @param isRequired A boolean flag indicating whether the element is required.
+ * Defaults to true.
+ * @return A double value representing the content of the target XML element, or
+ * 0. if the element is not found.
+ */
 double JetScapeXML::GetElementDouble(std::initializer_list<const char *> path,
                                      bool isRequired /* = true */) {
   tinyxml2::XMLElement *element = GetElement(path, isRequired);
@@ -275,7 +432,15 @@ double JetScapeXML::GetElementDouble(std::initializer_list<const char *> path,
   }
 }
 
-//________________________________________________________________
+/**
+ * @brief Overloaded stream insertion operator for outputting an initializer
+ * list of C-style strings. Prints the XML element path name.
+ *
+ * @param os The output stream to which the initializer list will be written.
+ * @param path The initializer list of C-style strings to be output.
+ * @return A reference to the output stream after the initializer list has been
+ * written.
+ */
 std::ostream &operator<<(std::ostream &os,
                          std::initializer_list<const char *> path) {
   int i = 0;
--- src/framework/JetScapeXML.h
+++ src/framework/JetScapeXML.h
@@ -24,6 +24,11 @@
 
 #include "tinyxml2.h"
 
+using std::runtime_error;
+using std::string;
+
+namespace Jetscape {
+
 /**
  * @class JetScapeXML
  * @brief JetScape XML init reader class (meant as singleton)
@@ -32,87 +37,320 @@
  * file, and a User file.
  *
  */
-
-using std::runtime_error;
-using std::string;
-
-namespace Jetscape {
-
 class JetScapeXML {
  public:
   static JetScapeXML *Instance();
 
-  // Master file: These functions are deprecated. Users should use the Main
-  // functions instead. These functions have been updated to use the 'main'
-  // instead of 'master' variables
+  /**
+   * @note Master file: These functions are deprecated. Users should use
+   * the Main functions instead. These functions have been updated to use
+   * the 'main' instead of 'master' variables
+   */
 
+  /**
+   * @brief Get the root element of the master XML file.
+   * @return The root element of the master XML file.
+   * @deprecated Use GetXMLRootMain() instead.
+   */
   tinyxml2::XMLElement *GetXMLRootMaster() { return xml_root_main; }
+
+  /**
+   * @brief Get the XML document of the master XML file.
+   * @return The XML document of the master XML file.
+   * @deprecated Use GetXMLDocumentMain() instead.
+   */
   tinyxml2::XMLDocument &GetXMLDocumentMaster() { return xml_doc_main; }
+
+  /**
+   * @brief Get an XML element from the master XML file.
+   * @param path The path to the XML element.
+   * @return The XML element at the specified path.
+   * @deprecated Use GetXMLElementMain() instead.
+   */
   tinyxml2::XMLElement *GetXMLElementMaster(
       std::initializer_list<const char *> &path);
 
+  /**
+   * @brief Set the name of the master XML file.
+   * @param m_name The name of the master XML file.
+   * @deprecated Use SetXMLMainFileName() instead.
+   */
   void SetXMLMasterFileName(string m_name) { xml_main_file_name = m_name; }
+
+  /**
+   * @brief Get the name of the master XML file.
+   * @return The name of the master XML file.
+   * @deprecated Use GetXMLMainFileName() instead.
+   */
   std::string GetXMLMasterFileName() { return xml_main_file_name; }
+
+  /**
+   * @brief Check if the master XML file is open.
+   * @return True if the master XML file is open, false otherwise.
+   * @deprecated Use IsMainFileOpen() instead.
+   */
   bool IsMasterFileOpen() { return xml_main_file_open; }
 
+  /**
+   * @brief Open the master XML file.
+   * @deprecated Use OpenXMLMainFile() instead.
+   */
   void OpenXMLMasterFile();
+
+  /**
+   * @brief Open the master XML file with the specified name.
+   * @param m_name The name of the master XML file.
+   * @deprecated Use OpenXMLMainFile() instead.
+   */
   void OpenXMLMasterFile(string m_name);
 
   // Main file:
 
+  /**
+   * @brief Get the root element of the main XML file.
+   * @return The root element of the main XML file.
+   */
   tinyxml2::XMLElement *GetXMLRootMain() const { return xml_root_main; }
+
+  /**
+   * @brief Get the XML document of the main XML file.
+   * @return The XML document of the main XML file.
+   */
   tinyxml2::XMLDocument &GetXMLDocumentMain() { return xml_doc_main; }
+
+  /**
+   * @brief Get an XML element from the main XML file.
+   * @param path The path to the XML element.
+   * @return The XML element at the specified path.
+   */
   tinyxml2::XMLElement *GetXMLElementMain(
       std::initializer_list<const char *> &path);
 
+  /**
+   * @brief Set the name of the main XML file.
+   * @param m_name The name of the main XML file.
+   */
   void SetXMLMainFileName(string m_name) { xml_main_file_name = m_name; }
+
+  /**
+   * @brief Get the name of the main XML file.
+   * @return The name of the main XML file.
+   */
   std::string GetXMLMainFileName() const { return xml_main_file_name; }
+
+  /**
+   * @brief Check if the main XML file is open.
+   * @return True if the main XML file is open, false otherwise.
+   */
   bool IsMainFileOpen() const { return xml_main_file_open; }
 
+  /**
+   * @brief Opens the main XML file for JetScape.
+   *
+   * This function attempts to open the main XML file specified by the
+   * GetXMLMainFileName() function. If the file is successfully opened,
+   * it sets the xml_root_main to the first "jetscape" element in the file.
+   * If the main file cannot be opened, it attempts to open a deprecated
+   * default master XML file located at "../config/jetscape_master.xml".
+   * If neither file can be opened, the function logs a warning and exits
+   * the program with an error code.
+   *
+   * @note The function sets the xml_main_file_open flag to true if the
+   * main XML file is successfully opened.
+   */
   void OpenXMLMainFile();
+
+  /**
+   * @brief Opens the main XML file with the given name.
+   *
+   * This function sets the main XML file name using the provided string
+   * and then opens the XML file.
+   *
+   * @param m_name The name of the XML file to be opened.
+   */
   void OpenXMLMainFile(string m_name);
 
   // User file
 
+  /**
+   * @brief Get the root element of the user XML file.
+   * @return The root element of the user XML file.
+   */
   tinyxml2::XMLElement *GetXMLRootUser() { return xml_root_user; }
+
+  /**
+   * @brief Get the XML document of the user XML file.
+   * @return The XML document of the user XML file.
+   */
   tinyxml2::XMLDocument &GetXMLDocumentUser() { return xml_doc_user; }
+
+  /**
+   * @brief Get an XML element from the user XML file.
+   * @param path The path to the XML element.
+   * @return The XML element at the specified path.
+   */
   tinyxml2::XMLElement *GetXMLElementUser(
       std::initializer_list<const char *> &path);
 
+  /**
+   * @brief Set the name of the user XML file.
+   * @param m_name The name of the user XML file.
+   */
   void SetXMLUserFileName(string m_name) { xml_user_file_name = m_name; }
+
+  /**
+   * @brief Get the name of the user XML file.
+   * @return The name of the user XML file.
+   */
   std::string GetXMLUserFileName() { return xml_user_file_name; }
+
+  /**
+   * @brief Check if the user XML file is open.
+   * @return True if the user XML file is open, false otherwise.
+   */
   bool IsUserFileOpen() { return xml_user_file_open; }
 
+  /**
+   * @brief Opens the XML user file and loads its content.
+   *
+   * This function attempts to open and load the XML user file specified by
+   * GetXMLUserFileName(). If the file is successfully opened and parsed, it
+   * sets the xml_root_user to the first "jetscape" element in the file. If the
+   * file cannot be opened or parsed, or if the root element is not found, the
+   * function logs an appropriate warning message and terminates the program.
+   *
+   * The function ensures that the XML user file is only opened once by checking
+   * the xml_user_file_open flag.
+   *
+   * @note This function will terminate the program if the XML user file cannot
+   * be opened or parsed correctly, or if the root element is not found.
+   */
   void OpenXMLUserFile();
+
+  /**
+   * @brief Opens an XML user file.
+   *
+   * This function sets the XML user file name using the provided string
+   * and then opens the XML user file.
+   *
+   * @param m_name The name of the XML user file to be opened.
+   */
   void OpenXMLUserFile(string m_name);
 
-  // Helper functions for XML parsing/
-  // Look first in user XML file for a parameter, and if not found look in the
-  // main XML file.
+  /**
+   * @note Helper functions for XML parsing
+   * Look first in user XML file for a parameter, and if not found
+   * look in the main XML file.
+   */
+
+  /**
+   * @brief Retrieves an XML element from the user or main XML file based on the
+   * provided path.
+   *
+   * This function attempts to find an XML element specified by the given path.
+   * It first searches in the user XML file, and if not found, it searches in
+   * the main XML file. If the element is required and not found in either file,
+   * the function logs a warning and terminates the program.
+   *
+   * @param path An initializer list of const char* representing the path to the
+   * desired XML element.
+   * @param isRequired A boolean flag indicating whether the XML element is
+   * required. Defaults to true.
+   * @return A pointer to the found tinyxml2::XMLElement, or nullptr if the
+   * element is not found and isRequired is false.
+   */
   tinyxml2::XMLElement *GetElement(std::initializer_list<const char *> path,
                                    bool isRequired = true);
+
+  /**
+   * @brief Retrieves the text content of an XML element specified by a path.
+   *
+   * This function navigates through the XML structure using the provided path
+   * and returns the text content of the target element. If the element is not
+   * found and isRequired is true, an exception may be thrown by GetElement.
+   * If the element is not found and isRequired is false, an empty string is
+   * returned.
+   *
+   * @param path An initializer list of const char* representing the path to the
+   * target element.
+   * @param isRequired A boolean flag indicating whether the element is
+   * required. Defaults to true.
+   * @return A std::string containing the text content of the target XML
+   * element, or an empty string if the element is not found and isRequired is
+   * false.
+   */
   std::string GetElementText(std::initializer_list<const char *> path,
                              bool isRequired = true);
+
+  /**
+   * @brief Retrieves the int value of an XML element specified by the given
+   * path.
+   *
+   * This function navigates through the XML structure using the provided path
+   * and retrieves the int value of the target element.
+   *
+   * @param path An initializer list of const char pointers representing the
+   * path to the target element.
+   * @param isRequired A boolean flag indicating whether the element is
+   * required. Defaults to true.
+   * @return An int value representing the content of the target XML element, or
+   * 0. if the element is not found.
+   */
   int GetElementInt(std::initializer_list<const char *> path,
                     bool isRequired = true);
+
+  /**
+   * @brief Retrieves the double value of an XML element specified by the given
+   * path.
+   *
+   * This function navigates through the XML structure using the provided path
+   * and retrieves the double value of the target element.
+   *
+   * @param path An initializer list of const char pointers representing the
+   * path to the target element.
+   * @param isRequired A boolean flag indicating whether the element is
+   * required. Defaults to true.
+   * @return A double value representing the content of the target XML element,
+   * or 0. if the element is not found.
+   */
   double GetElementDouble(std::initializer_list<const char *> path,
                           bool isRequired = true);
 
  private:
+  /**
+   * @brief Default constructor for JetScapeXML.
+   *
+   * This constructor initializes the JetScapeXML class with default values.
+   */
   JetScapeXML() {
     xml_main_file_name = "";
     xml_main_file_open = false;
     xml_user_file_name = "";
     xml_user_file_open = false;
   };
+
+  /**
+   * @brief Copy constructor for JetScapeXML.
+   *
+   * This constructor creates a new instance of JetScapeXML by copying an
+   * existing instance.
+   *
+   * @param other The JetScapeXML instance to copy from.
+   */
   JetScapeXML(JetScapeXML const &){};
+
+  /**
+   * @brief Singleton instance of the JetScapeXML class.
+   */
   static JetScapeXML *m_pInstance;
 
   // Main file
 
-  tinyxml2::XMLElement
-      *xml_root_main;  // use unique pointer here instead of raw pointer (check
-                       // with tinyxml interface)
+  /**
+   * @note use unique pointer here instead of raw pointer
+   * (check with tinyxml interface)
+   */
+  tinyxml2::XMLElement *xml_root_main;
   tinyxml2::XMLDocument xml_doc_main;
 
   std::string xml_main_file_name;
@@ -120,16 +358,26 @@ class JetScapeXML {
 
   // User file
 
-  tinyxml2::XMLElement
-      *xml_root_user;  // use unique pointer here instead of raw pointer (check
-                       // with tinyxml interface)
+  /**
+   * @note use unique pointer here instead of raw pointer
+   * (check with tinyxml interface)
+   */
+  tinyxml2::XMLElement *xml_root_user;
   tinyxml2::XMLDocument xml_doc_user;
 
   std::string xml_user_file_name;
   bool xml_user_file_open;
 };
 
-// Print the XML element path name
+/**
+ * @brief Overloaded stream insertion operator for outputting an initializer
+ * list of C-style strings. Prints the XML element path name.
+ *
+ * @param os The output stream to which the initializer list will be written.
+ * @param path The initializer list of C-style strings to be output.
+ * @return A reference to the output stream after the initializer list has been
+ * written.
+ */
 std::ostream &operator<<(std::ostream &os,
                          std::initializer_list<const char *> path);
 
