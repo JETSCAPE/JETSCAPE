03c51b1364aac629dfd3ac2c66d36ad86fec57a3
Cameron Parker <cameron.parker@tamu.edu>
2025-08-19 00:21:41 -0500

    easy default running script

---
+++ run-default.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#p3 make-ee-bins.py -d /data/rjfgroup/rjf01/cameron.parker/builds/JETSCAPE/designs/default.txt -n default-hybrid/LEP -p
+p3 make-pp-bins.py -d /data/rjfgroup/rjf01/cameron.parker/builds/JETSCAPE/designs/default.txt -n colored-default/LHC2760 -p
+p3 make-pp-bins.py -d /data/rjfgroup/rjf01/cameron.parker/builds/JETSCAPE/designs/default.txt -n colored-default/LHC13000 -p
+p3 make-pp-bins.py -d /data/rjfgroup/rjf01/cameron.parker/builds/JETSCAPE/designs/default.txt -n colored-default/RHIC -p
